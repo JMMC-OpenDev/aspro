ef13d59e788a9420d7242b92d9c669616ff55125
bourgesl <bourges.laurent@gmail.com>
2009-10-20 15:49:50 +0000

    class to store constant values

---
+++ src/fr/jmmc/aspro/AsproConstants.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fr.jmmc.aspro;
+
+/**
+ * This class gathers main constant values
+ * @author bourgesl
+ */
+public class AsproConstants {
+
+  /** EPOCH J2000 */
+  public final static float EPOCH_J2000 = 2000.f;
+
+  /** fixed earth radius constant from ASPRO Fortran code */
+  public final static double EARTH_RADIUS = 6367435d;
+
+
+
+}
