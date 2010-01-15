d24f5179a7208aece98c1c95146db7e4f902c6a9
bourgesl <bourges.laurent@gmail.com>
2010-01-15 13:49:32 +0000

    comments

--- src/fr/jmmc/aspro/AsproConstants.java
+++ src/fr/jmmc/aspro/AsproConstants.java
@@ -1,11 +1,14 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: AsproConstants.java,v 1.3 2010-01-08 16:51:18 bourgesl Exp $"
+ * "@(#) $Id: AsproConstants.java,v 1.4 2010-01-15 13:49:32 bourgesl Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.3  2010/01/08 16:51:18  bourgesl
+ * initial uv coverage
+ *
  */
 package fr.jmmc.aspro;
 
@@ -13,20 +16,23 @@ package fr.jmmc.aspro;
  * This class gathers main constant values
  * @author bourgesl
  */
-public class AsproConstants {
+public interface AsproConstants {
 
+  /* DEV */
   /** debug mode flag */
   public final static boolean DEBUG_MODE = true;
-
   /** chart : enables the zoom in / out */
   public final static boolean ENABLE_ZOOM = true;
-  
+
+  /* ASTRO constants */
   /** EPOCH J2000 */
   public final static float EPOCH_J2000 = 2000.f;
   /** fixed earth radius constant from ASPRO Fortran code */
   public final static double EARTH_RADIUS = 6367435d;
 
+  /* UI Defaults */
   /** default minimum elevation = 20 degrees */
   public static final double DEFAULT_MIN_ELEVATION = 20d;
-
+  /** default sampling periodicity = 40 minutes */
+  public static final double DEFAULT_SAMPLING_PERIOD = 40d;
 }
