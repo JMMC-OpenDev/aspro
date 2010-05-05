90e1081bb76e1c285c5ea3bb82e7e52d5068f230
bourgesl <bourges.laurent@gmail.com>
2010-05-05 14:31:31 +0000

    Generate the date constraints if the night restrictions are active Generate the LST intervals by calling again the ObservabilityService (elevation >30°, LST) with ha Min / Max restriction

--- src/fr/jmmc/aspro/ob/ExportOBVLTI.java
+++ src/fr/jmmc/aspro/ob/ExportOBVLTI.java
@@ -1,11 +1,14 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: ExportOBVLTI.java,v 1.2 2010-04-14 13:09:59 bourgesl Exp $"
+ * "@(#) $Id: ExportOBVLTI.java,v 1.3 2010-05-05 14:31:31 bourgesl Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2010/04/14 13:09:59  bourgesl
+ * first minimal OB for MIDI
+ *
  * Revision 1.1  2010/04/13 15:55:14  bourgesl
  * extracted common part for VLTI
  *
@@ -34,13 +37,23 @@ package fr.jmmc.aspro.ob;
 import edu.dartmouth.AstroSkyCalc;
 import fr.jmmc.aspro.AsproConstants;
 import fr.jmmc.aspro.model.ObservationManager;
+import fr.jmmc.aspro.model.Range;
+import fr.jmmc.aspro.model.observability.DateTimeInterval;
+import fr.jmmc.aspro.model.observability.ObservabilityData;
+import fr.jmmc.aspro.model.observability.StarData;
 import fr.jmmc.aspro.model.oi.ObservationSetting;
 import fr.jmmc.aspro.model.oi.Station;
 import fr.jmmc.aspro.model.oi.Target;
+import fr.jmmc.aspro.service.ObservabilityService;
 import java.io.File;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
 import java.util.logging.Level;
+import javax.xml.datatype.XMLGregorianCalendar;
 
 /**
  * This class generates the common part of observing blocks for the VLTI
@@ -57,10 +70,20 @@ public class ExportOBVLTI {
   protected final static NumberFormat df3 = new DecimalFormat("0.000");
   /** double formatter for PM */
   protected final static NumberFormat df6 = new DecimalFormat("0.000000");
+  /** minimum elevation for LST Time ranges = 30 deg */
+  public final static double VLTI_MIN_ELEV = 30d;
+  /** absolute_times_list template value */
+  public final static String VAL_ABS_TIME_LIST = "{<DATE>T00:00:00 <DATE>T00:00:00 1}";
+  /** date keyword for absolute_times_list template value */
+  public final static String VAL_ABS_TIME_LIST_DATE = "<DATE>";
 
   /* keywords */
   /** keyword - name */
   public final static String KEY_NAME = "<NAME>";
+  /** keyword - absolute_times_list */
+  public final static String KEY_ABS_TIME_LIST = "<ABS-TIME-LIST>";
+  /** keyword - STTimeIntervals */
+  public final static String KEY_LST_INTERVALS = "<LST-INTERVALS>";
   /** keyword - comments */
   public final static String KEY_COMMENTS = "<COMMENTS>";
   /** keyword - ra */
@@ -92,10 +115,12 @@ public class ExportOBVLTI {
   /**
    * Generate the OB file for the given target.
    * According to the instrument defined in the observation, it uses ExportOBAmber or ExportOBMidi.
-   * @param targetName target to process
    * @param file file to save
+   * @param targetName target to process
+   * @param haMin HA min in decimal hours
+   * @param haMax HA max in decimal hours
    */
-  public static void process(final String targetName, final File file) {
+  public static void process(final File file, final String targetName, final double haMin, final double haMax) {
     if (logger.isLoggable(Level.FINE)) {
       logger.fine("process " + targetName + " to " + file);
     }
@@ -108,9 +133,9 @@ public class ExportOBVLTI {
     final String instrumentName = observation.getInstrumentConfiguration().getName();
 
     if (AsproConstants.INS_AMBER.equals(instrumentName)) {
-      ExportOBAmber.generate(file, observation, target);
+      ExportOBAmber.generate(file, observation, target, haMin, haMax);
     } else if (AsproConstants.INS_MIDI.equals(instrumentName)) {
-      ExportOBMidi.generate(file, observation, target);
+      ExportOBMidi.generate(file, observation, target, haMin, haMax);
     } else {
       throw new IllegalArgumentException("The application can not generate an Observing Block for this instrument [" + instrumentName + "] !");
     }
@@ -122,9 +147,12 @@ public class ExportOBVLTI {
    * @param fileName OB file name
    * @param observation observation settings
    * @param target target to process
+   * @param haMin HA min in decimal hours
+   * @param haMax HA max in decimal hours
    * @return processed template
    */
-  protected static String processCommon(final String template, final String fileName, final ObservationSetting observation, final Target target) {
+  protected static String processCommon(final String template, final String fileName, final ObservationSetting observation, final Target target,
+                                        final double haMin, final double haMax) {
     if (logger.isLoggable(Level.FINE)) {
       logger.fine("processCommon " + target.getName());
     }
@@ -145,6 +173,10 @@ public class ExportOBVLTI {
     }
 
     document = document.replaceFirst(KEY_NAME, name);
+
+    // --- Date / Time constraints ---
+
+    document = processDateTime(document, observation, target, haMin, haMax);
 
     // --- Target information ---
 
@@ -185,5 +217,158 @@ public class ExportOBVLTI {
     sb.deleteCharAt(sb.length() - 1);
 
     return sb.toString();
+  }
+
+  /**
+   * Process the Date / Time constraints for the given target
+   * @param document OB document
+   * @param observation observation settings
+   * @param target target to process
+   * @param haMin HA min in decimal hours
+   * @param haMax HA max in decimal hours
+   * @return processed template
+   */
+  private static String processDateTime(String document, final ObservationSetting observation, final Target target,
+                                        final double haMin, final double haMax) {
+
+    String lstTimeIntervals = "";
+    String absTimeList = "";
+
+    // Compute Observability data with min elevation = 30 deg (date and night restrictions depend on the current observation) :
+    final ObservabilityService os = new ObservabilityService(observation, target, VLTI_MIN_ELEV);
+    final ObservabilityData obsData = os.compute();
+
+    final StarData starData = obsData.getStarData(target.getName());
+    if (starData != null) {
+      final List<Range> obsRangesHA = starData.getObsRangesHA();
+
+      if (logger.isLoggable(Level.FINE)) {
+        logger.fine("obsRangesHA = " + obsRangesHA);
+      }
+
+      if (obsRangesHA != null) {
+        // target is observable :
+
+        final List<Range> limRangesHA = restrictRange(obsRangesHA, haMin, haMax);
+
+        if (logger.isLoggable(Level.FINE)) {
+          logger.fine("limRangesHA = " + limRangesHA);
+        }
+
+        final List<DateTimeInterval> lstRanges = os.convertHARangesToDateInterval(limRangesHA, starData.getPrecRA());
+        if (logger.isLoggable(Level.FINE)) {
+          logger.fine("lst ranges = " + lstRanges);
+        }
+
+        if (!lstRanges.isEmpty()) {
+          // STTimeIntervals are expressed in seconds
+          // sample "4980:17760;"
+
+          lstTimeIntervals = convertLstRanges(lstRanges);
+
+          // if night restriction is active, LST ranges are only valid for the observation date :
+          if (observation.getWhen().isNightRestriction()) {
+            // get observation date :
+            final XMLGregorianCalendar cal = observation.getWhen().getDate();
+            if (logger.isLoggable(Level.FINE)) {
+              logger.fine("obs Date = " + cal);
+            }
+
+            // define Date constraint in eso date format is '2010-05-04' :
+
+            // {<DATE>T00:00:00 <DATE>T00:00:00 1}
+            absTimeList = VAL_ABS_TIME_LIST.replaceAll(VAL_ABS_TIME_LIST_DATE, cal.toString());
+          }
+        }
+      }
+    }
+
+    // replace values :
+    if (logger.isLoggable(Level.FINE)) {
+      logger.fine("lstTimeIntervals = " + lstTimeIntervals);
+    }
+
+    document = document.replaceAll(KEY_LST_INTERVALS, lstTimeIntervals);
+
+    if (logger.isLoggable(Level.FINE)) {
+      logger.fine("absTimeList = " + absTimeList);
+    }
+
+    document = document.replaceAll(KEY_ABS_TIME_LIST, absTimeList);
+
+    return document;
+  }
+
+  private static List<Range> restrictRange(final List<Range> ranges, final double min, final double max) {
+    final List<Range> intervals = new ArrayList<Range>(ranges.size());
+
+    double start, end;
+    for (Range range : ranges) {
+      start = range.getMin();
+      end = range.getMax();
+
+      if (start >= min) {
+        if (end <= max) {
+          // interval in inside [min;max]
+          intervals.add(range);
+        } else {
+          if (start > max) {
+            // two points over max : skip
+          } else {
+            // end occurs after max :
+            intervals.add(new Range(start, max));
+          }
+        }
+      } else {
+        // start occurs before min :
+        if (end < min) {
+          // two points before min : skip
+        } else if (end > max) {
+          // two points overlapping [min;max] : keep
+          intervals.add(new Range(min, max));
+        } else {
+          intervals.add(new Range(min, end));
+        }
+      }
+    }
+    return intervals;
+  }
+
+  /**
+   * Convert LST date intervals to STTimeIntervals format i.e. 'date1_in_seconds:date2_in_seconds;...'
+   * @param dateIntervals LST intervals
+   * @return String value
+   */
+  private static String convertLstRanges(final List<DateTimeInterval> dateIntervals) {
+    final StringBuilder sb = new StringBuilder(32);
+
+    final Calendar cal = new GregorianCalendar();
+
+    for (DateTimeInterval interval : dateIntervals) {
+      cal.setTime(interval.getStartDate());
+      // upper minute :
+      sb.append(convertDateToSeconds(cal, 1)).append(':');
+
+      cal.setTime(interval.getEndDate());
+      // lower minute :
+      sb.append(convertDateToSeconds(cal, -1)).append(';');
+    }
+
+    return sb.toString();
+  }
+
+  /**
+   * Convert a calendar time to seconds rounding to the lower or upper minute
+   * according to the given sign
+   * @param cal calendar time
+   * @param sign +/- 1
+   * @return seconds
+   */
+  private static int convertDateToSeconds(final Calendar cal, final int sign) {
+
+    final int h = cal.get(Calendar.HOUR_OF_DAY);
+    final int m = cal.get(Calendar.MINUTE);
+
+    return h * 3600 + (m + sign) * 60;
   }
 }
