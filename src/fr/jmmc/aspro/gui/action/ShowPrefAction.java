417552f5f8aa87153347f54f4c0d5ceab4dd6623
bourgesl <bourges.laurent@gmail.com>
2010-05-21 14:26:41 +0000

    do not keep an reference to the PreferencesView

--- src/fr/jmmc/aspro/gui/action/ShowPrefAction.java
+++ src/fr/jmmc/aspro/gui/action/ShowPrefAction.java
@@ -1,31 +1,31 @@
 package fr.jmmc.aspro.gui.action;
 
-import fr.jmmc.aspro.gui.*;
+import fr.jmmc.aspro.gui.PreferencesView;
 import fr.jmmc.mcs.util.RegisteredAction;
 import java.awt.event.ActionEvent;
+import java.util.logging.Level;
 
 public class ShowPrefAction extends RegisteredAction {
 
-    private final static String className = "fr.jmmc.mf.gui.actions.ShowPrefAction";
-    /** Class logger */
-    static java.util.logging.Logger logger = java.util.logging.Logger.getLogger(
-            className);
-    /** Preferences view */
-    PreferencesView preferencesView;   
+  /** default serial UID for Serializable interface */
+  private static final long serialVersionUID = 1;
+  private final static String className = "fr.jmmc.aspro.gui.action.ShowPrefAction";
+  /** Class logger */
+  private final static java.util.logging.Logger logger = java.util.logging.Logger.getLogger(
+          className);
 
-    public ShowPrefAction() {
-        super(className, "showPreferences");        
-        flagAsPreferenceAction();
-        preferencesView = new PreferencesView();
-    }
+  public ShowPrefAction() {
+    super(className, "showPreferences");
+    flagAsPreferenceAction();
+  }
 
-    public void actionPerformed(ActionEvent e) {
-        try {
-            preferencesView.setVisible(true);
-            logger.fine("Showing preferences");
-        } catch (Exception exc) {
-            // @todo handle this error at user level
-            exc.printStackTrace();
-        }
+  public void actionPerformed(ActionEvent e) {
+    try {
+      new PreferencesView().setVisible(true);
+      logger.fine("Showing preferences");
+    } catch (Exception ex) {
+      // @todo handle this error at user level
+      logger.log(Level.SEVERE, "actionPerformed", ex);
     }
+  }
 }
