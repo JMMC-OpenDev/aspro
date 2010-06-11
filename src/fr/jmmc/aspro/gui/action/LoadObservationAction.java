a7bb60b314845fb2838ad15470743efb73ecef87
bourgesl <bourges.laurent@gmail.com>
2010-06-11 13:49:08 +0000

    flagged as open action handle the action event's file coming from ActionRegistar (open action on application startup)

--- src/fr/jmmc/aspro/gui/action/LoadObservationAction.java
+++ src/fr/jmmc/aspro/gui/action/LoadObservationAction.java
@@ -1,11 +1,14 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: LoadObservationAction.java,v 1.4 2010-06-09 12:54:12 bourgesl Exp $"
+ * "@(#) $Id: LoadObservationAction.java,v 1.5 2010-06-11 13:49:08 bourgesl Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.4  2010/06/09 12:54:12  bourgesl
+ * javadoc
+ *
  * Revision 1.3  2010/04/02 10:06:29  bourgesl
  * add missing xml extension to the file name
  *
@@ -20,6 +23,7 @@ package fr.jmmc.aspro.gui.action;
 
 import fr.jmmc.aspro.model.ObservationManager;
 import fr.jmmc.mcs.gui.StatusBar;
+import fr.jmmc.mcs.util.ActionRegistrar;
 import java.awt.event.ActionEvent;
 import java.io.File;
 import java.util.logging.Level;
@@ -46,13 +50,14 @@ public class LoadObservationAction extends ObservationFileAction {
    */
   public LoadObservationAction() {
     super(className, actionName);
+    flagAsOpenAction();
   }
 
   /**
    * Handle the action event
    * @param evt action event
    */
-  public void actionPerformed(final ActionEvent e) {
+  public void actionPerformed(final ActionEvent evt) {
     if (logger.isLoggable(Level.FINE)) {
       logger.fine("actionPerformed");
     }
@@ -60,20 +65,26 @@ public class LoadObservationAction extends ObservationFileAction {
 
     File file = null;
 
-    final JFileChooser fileChooser = new JFileChooser();
-    fileChooser.setFileFilter(getFileFilter());
-
-    if (this.getLastDir() != null) {
-      fileChooser.setCurrentDirectory(new File(this.getLastDir()));
-    }
-
-    fileChooser.setDialogTitle("Load observation settings");
-
-    final int returnVal = fileChooser.showOpenDialog(null);
-    if (returnVal == JFileChooser.APPROVE_OPTION) {
-      file = fileChooser.getSelectedFile();
+    // If the action was automatically triggered from App launch
+    if (evt.getSource() == ActionRegistrar.getInstance()) {
+      file = new File(evt.getActionCommand());
     } else {
-      file = null;
+
+      final JFileChooser fileChooser = new JFileChooser();
+      fileChooser.setFileFilter(getFileFilter());
+
+      if (this.getLastDir() != null) {
+        fileChooser.setCurrentDirectory(new File(this.getLastDir()));
+      }
+
+      fileChooser.setDialogTitle("Load observation settings");
+
+      final int returnVal = fileChooser.showOpenDialog(null);
+      if (returnVal == JFileChooser.APPROVE_OPTION) {
+        file = fileChooser.getSelectedFile();
+      } else {
+        file = null;
+      }
     }
 
     // If a file was defined (No cancel in the dialog)
