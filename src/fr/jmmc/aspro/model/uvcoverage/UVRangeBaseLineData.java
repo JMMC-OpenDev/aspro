666150dfddf04399177076967b233c8ae54dfa25
bourgesl <bourges.laurent@gmail.com>
2010-06-25 14:14:57 +0000

    added baseline, HA, UV for WMin / WMax to compute easily the OI_VIS table

--- src/fr/jmmc/aspro/model/uvcoverage/UVRangeBaseLineData.java
+++ src/fr/jmmc/aspro/model/uvcoverage/UVRangeBaseLineData.java
@@ -1,48 +1,89 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: UVRangeBaseLineData.java,v 1.1 2010-01-08 16:50:53 bourgesl Exp $"
+ * "@(#) $Id: UVRangeBaseLineData.java,v 1.2 2010-06-25 14:14:57 bourgesl Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2010/01/08 16:50:53  bourgesl
+ * initial uv coverage
+ *
  */
 package fr.jmmc.aspro.model.uvcoverage;
+
+import fr.jmmc.aspro.model.BaseLine;
 
 /**
  * This class extends the UVBaseLineData class to define the second uv coordinates.
  * That defines an uv range corresponding to the minimal and maximal wavelength of the focal instrument (mode).
  * @author bourgesl
  */
-public class UVRangeBaseLineData extends UVBaseLineData {
+public final class UVRangeBaseLineData extends UVBaseLineData {
 
+  /** base line (used by OIFits) */
+  private final BaseLine bl;
+  /** decimal hour angle (used by OIFits) */
+  private double[] ha = null;
+  /** u coordinates for the minimal wavelength */
+  private double[] uWMin = null;
+  /** v coordinates for the minimal wavelength */
+  private double[] vWMin = null;
   /** u coordinates for the maximal wavelength */
-  private double[] u2 = null;
+  private double[] uWMax = null;
   /** v coordinates for the maximal wavelength */
-  private double[] v2 = null;
+  private double[] vWMax = null;
 
   /**
    * Constructor
-   * @param name baseline name
+   * @param bl baseline
    */
-  public UVRangeBaseLineData(final String name) {
-    super(name);
+  public UVRangeBaseLineData(final BaseLine bl) {
+    super(bl.getName());
+    this.bl = bl;
   }
 
-  public double[] getU2() {
-    return u2;
+  public BaseLine getBaseLine() {
+    return this.bl;
   }
 
-  public void setU2(double[] u2) {
-    this.u2 = u2;
+  public double[] getHA() {
+    return ha;
   }
 
-  public double[] getV2() {
-    return v2;
+  public void setHA(final double[] ha) {
+    this.ha = ha;
   }
 
-  public void setV2(double[] v2) {
-    this.v2 = v2;
+  public double[] getUWMin() {
+    return uWMin;
   }
 
+  public void setUWMin(double[] uWMin) {
+    this.uWMin = uWMin;
+  }
+
+  public double[] getVWMin() {
+    return this.vWMin;
+  }
+
+  public void setVWMin(final double[] vWMin) {
+    this.vWMin = vWMin;
+  }
+
+  public double[] getUWMax() {
+    return this.uWMax;
+  }
+
+  public void setUWMax(final double[] uWMax) {
+    this.uWMax = uWMax;
+  }
+
+  public double[] getVWMax() {
+    return this.vWMax;
+  }
+
+  public void setVWMax(final double[] vWMax) {
+    this.vWMax = vWMax;
+  }
 }
