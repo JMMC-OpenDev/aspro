c8ca6a58e3b595f7c5cd00e304f4050611be5dd1
bourgesl <bourges.laurent@gmail.com>
2010-06-25 14:12:16 +0000

    code cleanup methods related to targets moved in AstoSkyCalcObservation Added methods to convert HA to JD

--- src/edu/dartmouth/AstroSkyCalc.java
+++ src/edu/dartmouth/AstroSkyCalc.java
@@ -1,11 +1,14 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: AstroSkyCalc.java,v 1.20 2010-06-17 10:02:51 bourgesl Exp $"
+ * "@(#) $Id: AstroSkyCalc.java,v 1.21 2010-06-25 14:12:16 bourgesl Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.20  2010/06/17 10:02:51  bourgesl
+ * fixed warning hints - mainly not final static loggers
+ *
  * Revision 1.19  2010/05/26 15:28:09  bourgesl
  * added method toString(ra, raDigits, dec, decDigits) to choose the number of digits
  *
@@ -26,8 +29,6 @@
 package edu.dartmouth;
 
 import edu.dartmouth.SunAlmanachTime.SunAlmanachType;
-import fr.jmmc.aspro.AsproConstants;
-import fr.jmmc.aspro.model.oi.AzEl;
 import fr.jmmc.aspro.model.oi.LonLatAlt;
 import fr.jmmc.aspro.util.AngleUtils;
 import java.util.ArrayList;
@@ -39,27 +40,28 @@ import java.util.TreeSet;
 import java.util.logging.Level;
 
 /**
- * This class uses JSkyCalc to perform several astronomical computations (LST time, sun ephemerids, target position with elevation and azimuth ...)
+ * This class uses JSkyCalc to perform several astronomical computations (sun ephemerids, LST/UTC time, JD time ...)
  * 
  * @author bourgesl
  */
-public class AstroSkyCalc {
+public final class AstroSkyCalc {
 
   /** Class Name */
   private static final String className_ = "edu.dartmouth.AstroSkyCalc";
   /** Class logger */
-  private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(
-          className_);
+  private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(className_);
   /** time ratio : lst to jd */
-  public static double LST_TO_JD = 24d * Const.SID_RATE;
+  public final static double LST_TO_JD = 24d * Const.SID_RATE;
+  /** one Day in LST expressed in JD day */
+  public final static double LST_DAY_IN_JD = AstroSkyCalc.lst2jd(24d);
+  /** 1 second in decimal hour */
+  private final static double SEC_IN_DEC_HOUR = 1d / 3600d;
 
   /* members */
   /** site location */
-  private Site site;
-  /** time / site info */
-  private WhenWhere when;
-  /** target info */
-  private Observation observation;
+  Site site;
+  /** jd corresponding to LST=00:00:00 for the observation date */
+  private double jdLst0;
 
   /**
    * Public Constructor
@@ -80,26 +82,29 @@ public class AstroSkyCalc {
     }
 
     // note : the given longitude is hours west in jSkyCalc :
+    // stdz = longitude (dec hours) is also the GMT offset (UTC)
     this.site = new Site(name,
             -AngleUtils.rad2hours(position.getLongitude()),
             Math.toDegrees(position.getLatitude()),
             position.getAltitude());
 
     if (logger.isLoggable(Level.FINE)) {
-      logger.fine("Site dump : " + this.site.name +
-              "\ntz offset : " + this.site.stdz +
-              "\nlongitude : " + this.site.longit.RoundedLongitString(1, ":", true) +
-              "\nlatitude  : " + this.site.lat.RoundedDecString(0, ":"));
+      logger.fine("Site dump : " + this.site.name
+              + "\ntz offset : " + this.site.stdz
+              + "\nlongitude : " + this.site.longit.RoundedLongitString(1, ":", true)
+              + "\nlatitude  : " + this.site.lat.RoundedDecString(0, ":"));
     }
   }
 
   /**
    * Define the observation date with a gregorian date (lenient) in UTC at 00:00
+   * and return the jd time value corresponding to LST=00:00:00 for the current date
    * @param year year
    * @param month month from [1-12]
    * @param day day in [1-31]
+   * @return jd corresponding to LST=00:00:00 for the current date
    */
-  public void defineDate(final int year, final int month, final int day) {
+  public double defineDate(final int year, final int month, final int day) {
     // yyyy mm dd hh:mm
     final String dateTime = year + " " + month + " " + day + " 00:00";
 
@@ -107,37 +112,41 @@ public class AstroSkyCalc {
       logger.fine("dateTime : " + dateTime);
     }
 
-    // UTC time :
-    final InstantInTime time = new InstantInTime(dateTime, this.site.stdz, this.site.use_dst, true);
+    // Define date as an UTC time :
+    final InstantInTime instant = new InstantInTime(dateTime, this.site.stdz, this.site.use_dst, true);
 
-    this.when = new WhenWhere(time, this.site);
+    final WhenWhere ww = new WhenWhere(instant, this.site);
 
     if (logger.isLoggable(Level.FINE)) {
-      dumpWhen(this.when, "When");
+      dumpWhen(ww, "When");
     }
+
+    // Find the julian date corresponding to the LST origin LST=00:00:00 for the given date :
+    this.jdLst0 = findJdForLst0(ww.when.jd);
+
+    return this.jdLst0;
   }
 
   /**
-   * Find the jd time value corresponding to the lst = 0 for the current date
+   * Return the jd time value corresponding to LST=00:00:00 for the current date
+   * @see #findJdForLst0(double)
    *
-   * Note : accurate +/-12 h within the given date
-   *
-   * @return jd corresponding to the lst = 0 for the current date
+   * @return jd corresponding to LST=00:00:00 for the current date
    */
-  public double findLst0() {
-    return findLst0(this.when.when.jd);
+  public double getJdForLst0() {
+    return this.jdLst0;
   }
 
   /**
-   * Find the jd time value corresponding to the lst = 0 for the given jd date
+   * Find the jd time value corresponding to LST=00:00:00 arround the given jd date
    *
    * Note : accurate +/-12 h within the given date
    *
    * @param jd julian date
    *
-   * @return jd corresponding to the lst = 0 for the given jd date
+   * @return jd corresponding to LST=00:00:00 for the given jd date
    */
-  public double findLst0(final double jd) {
+  public double findJdForLst0(final double jd) {
 
     final WhenWhere ww = new WhenWhere(jd, this.site);
 
@@ -145,7 +154,7 @@ public class AstroSkyCalc {
     double error = ww.sidereal;
     int n = 0;
 
-    while (error > 1e-3 && n < 9) {
+    while (error > SEC_IN_DEC_HOUR && n < 9) {
 
       if (error > 12d) {
         error = 24d - error;
@@ -153,7 +162,7 @@ public class AstroSkyCalc {
         error *= -1d;
       }
 
-      ww.ChangeWhen(ww.when.jd + (error / 24.d));
+      ww.ChangeWhen(ww.when.jd + (error / 24d));
 
 //    dumpWhen(this.when, "When");
 
@@ -163,10 +172,28 @@ public class AstroSkyCalc {
     }
 
     if (logger.isLoggable(Level.FINE)) {
-      dumpWhen(ww, "LST=0");
+      dumpWhen(ww, "LST=00:00:00");
     }
 
     return ww.when.jd;
+  }
+
+  /**
+   * Convert a julian date to a gregorian date (precise up to the second) in LST
+   * @param jd julian date
+   * @return Date object
+   */
+  public Date toDateLST(final double jd) {
+    return toDate(jd, true);
+  }
+
+  /**
+   * Convert a julian date to a gregorian date (precise up to the second) in UTC
+   * @param jd julian date
+   * @return Date object
+   */
+  public Date toDateUTC(final double jd) {
+    return toDate(jd, false);
   }
 
   /**
@@ -189,7 +216,8 @@ public class AstroSkyCalc {
       cal.set(Calendar.SECOND, (int) Math.round(ww.siderealobj.sex.second));
     } else {
       /* note : month is in range [0;11] in java Calendar */
-      cal.set(t.UTDate.year, t.UTDate.month - 1, t.UTDate.day, t.UTDate.timeofday.hour, t.UTDate.timeofday.minute, (int) Math.round(t.UTDate.timeofday.second));
+      cal.set(t.UTDate.year, t.UTDate.month - 1, t.UTDate.day,
+              t.UTDate.timeofday.hour, t.UTDate.timeofday.minute, (int) Math.round(t.UTDate.timeofday.second));
     }
     // fix milliseconds to 0 to be able to compare date instances :
     cal.set(Calendar.MILLISECOND, 0);
@@ -205,20 +233,20 @@ public class AstroSkyCalc {
   private void dumpWhen(final WhenWhere ww, final String label) {
     if (logger.isLoggable(Level.FINE)) {
       final InstantInTime t = ww.when;
-      logger.fine(label + " dump : " + t.jd +
-              "\nUT : " + t.UTDate.day +
-              "/" + t.UTDate.month +
-              "/" + t.UTDate.year +
-              " " + t.UTDate.timeofday.hour +
-              ":" + t.UTDate.timeofday.minute +
-              ":" + t.UTDate.timeofday.second +
-              "\nlst : " + ww.siderealobj.RoundedRAString(3, ":"));
+      logger.fine(label + " dump : " + t.jd
+              + "\nUT : " + t.UTDate.day
+              + "/" + t.UTDate.month
+              + "/" + t.UTDate.year
+              + " " + t.UTDate.timeofday.hour
+              + ":" + t.UTDate.timeofday.minute
+              + ":" + t.UTDate.timeofday.second
+              + "\nlst : " + ww.siderealobj.RoundedRAString(3, ":"));
     }
   }
 
   /**
    * Return the list of Sun events arround LST [0;24] +- 12h
-   * @param jdLst0 julian date corresponding to LST = 0
+   * @param jdLst0 julian date corresponding to LST=00:00:00 for the observation date
    * @return list of Sun events
    */
   public List<SunAlmanachTime> findSunRiseSet(final double jdLst0) {
@@ -243,11 +271,11 @@ public class AstroSkyCalc {
 
     for (int i = 0, len = sorted.size(); i < len; i++) {
       st = sorted.get(i);
-      /*
-      if (logger.isLoggable(Level.FINE)) {
-      dumpWhen(new WhenWhere(st.getJd(), this.site), st.getType().name());
-      }
-       */
+
+//      if (logger.isLoggable(Level.FINE)) {
+//        dumpWhen(new WhenWhere(st.getJd(), this.site), st.getType().name());
+//      }
+
       if (st.getJd() >= jd0) {
         if (i0 == -1) {
           // include previous event :
@@ -287,7 +315,9 @@ public class AstroSkyCalc {
    * @param jd julian date
    */
   private void addAlmanach(final TreeSet<SunAlmanachTime> ts, final double jd) {
-    final NightlyAlmanac na = new NightlyAlmanac(new WhenWhere(jd, this.site));
+    final WhenWhere ww = new WhenWhere(jd, this.site);
+
+    final NightlyAlmanac na = new NightlyAlmanac(ww);
 
     ts.add(new SunAlmanachTime(na.morningTwilight.when.jd, SunAlmanachType.SunTwlRise));
     ts.add(new SunAlmanachTime(na.sunrise.when.jd, SunAlmanachType.SunRise));
@@ -296,126 +326,40 @@ public class AstroSkyCalc {
   }
 
   /**
-   * Define a target by its RA/dec coordinates in degrees
-   * @param jdLst0 julian date corresponding to LST = 0
-   * @param ra right ascension (deg)
-   * @param dec declination (deg)
-   * @return double[] containing precessed ra (dec hours) and dec (deg) for the given jd date
+   * Convert a decimal hour angle in Julian day
+   * @param ha decimal hour angle
+   * @param precRA precessed target right ascension in decimal hours
+   * @return JD value
    */
-  public double[] defineTarget(final double jdLst0, final double ra, final double dec) {
+  public double convertHAToJD(final double ha, final double precRA) {
+    final double lst = precRA + ha;
 
-    // RA (decimal hours), DEC (degrees)
-    final Celest target = new Celest(AngleUtils.deg2hours(ra), dec, AsproConstants.EPOCH_J2000);
-
-    if (logger.isLoggable(Level.FINE)) {
-      logger.fine("Target [RA/DEC/EPOCH] :" + target.Alpha.RoundedRAString(3, ":") + " " + target.Delta.RoundedDecString(3, ":"));
-    }
-
-    // define jd :
-    this.when.ChangeWhen(jdLst0);
-
-    this.observation = new Observation(this.when, target);
-
-    return new double[]{this.observation.current.Alpha.value, this.observation.current.Delta.value};
+    // apply the sideral / solar ratio :
+    return this.jdLst0 + AstroSkyCalc.lst2jd(lst);
   }
 
   /**
-   * Return the current target position (azimuth / elevation) in degrees
-   * @param jd julian date
-   * @return azimuth (0 to north) / elevation in degrees
+   * Convert a Julian day in decimal hour angle
+   * Note : HA is not resticted to [-12;12]
+   * @param jd date time
+   * @param precRA precessed target right ascension in decimal hours
+   * @return ha decimal hour angle
    */
-  public AzEl getTargetPosition(final double jd) {
+  public double convertJDToHA(final double jd, final double precRA) {
+    // apply the sideral / solar ratio :
+    final double lst = AstroSkyCalc.jd2lst(jd - this.jdLst0);
 
-    this.observation.w.ChangeWhen(jd);
-    this.observation.ComputeSky();
-
-    /*
-    if (logger.isLoggable(Level.FINE)) {
-    dumpWhen(this.observation.w, "Target");
-    logger.fine("az|alt   : " + this.observation.azimuth + " " + this.observation.altitude);
-    }
-     */
-    return new AzEl(this.observation.azimuth, this.observation.altitude);
-  }
-
-  /**
-   * Log the minimum and maximum elevation for the current target / site
-   */
-  private void getTargetMinMaxAlt() {
-    final double[] minmax = Spherical.min_max_alt(this.site.lat.value, this.observation.current.Delta.value);
-
-    // degrees :
-    if (logger.isLoggable(Level.FINE)) {
-      logger.fine("min/max alt = " + minmax[0] + " / " + minmax[1]);
-    }
-  }
-
-  /**
-   * Computes the hour angle corresponding to the given elevation for the current target
-   * @param dec target declination (corrected for the given julian date)
-   * @param minElev min elevation (deg)
-   * @return hour angle (dec hours) or -1 if the target never reaches this elevation
-   */
-  public double getHAForElevation(final double dec, final double minElev) {
-
-    if (logger.isLoggable(Level.INFO)) {
-      getTargetMinMaxAlt();
-    }
-
-    final double ha = Spherical.ha_alt(dec, this.site.lat.value, minElev);
-
-    if (ha == -1000d) {
-      // never rise (target is never over the min elevation) :
-      return -1d;
-    }
-
-    if (ha == 1000d) {
-      // always rise (target is always over the min elevation) :
-      return 12d;
-    }
-
-    if (logger.isLoggable(Level.FINE)) {
-      logger.fine("ha = " + ha);
-    }
-
-    return ha;
+    return lst - precRA;
   }
 
   // static methods :
-  /**
-   * Return a string representation for RA (hms) and DEC (dms) with 3 digits
-   * @param ra right ascension in deg
-   * @param dec declination in deg
-   * @return string[] containing RA (hms) and DEC (dms)
-   */
-  public static String[] toString(final double ra, final double dec) {
-    return toString(ra, 3, dec, 3);
-  }
-
-  /**
-   * Return a string representation for RA (hms) and DEC (dms) with the given precision (number of digits)
-   * @param ra right ascension in deg
-   * @param raDigits ra digits
-   * @param dec declination in deg
-   * @param decDigits ra digits
-   * @return string[] containing RA (hms) and DEC (dms)
-   */
-  public static String[] toString(final double ra, final int raDigits, final double dec, final int decDigits) {
-    // RA (decimal hours), DEC (degrees)
-    final Celest target = new Celest(AngleUtils.deg2hours(ra), dec, AsproConstants.EPOCH_J2000);
-
-    return new String[]{
-              target.Alpha.RoundedRAString(raDigits, ":"),
-              target.Delta.RoundedDecString(decDigits, ":")
-            };
-  }
   /**
    * Convert lst hours to jd hours
    * @param lst dec hours in LST
    * @return dec hours in JD
    */
   public static double lst2jd(final double lst) {
-    return lst / AstroSkyCalc.LST_TO_JD;
+    return lst / LST_TO_JD;
   }
 
   /**
@@ -424,6 +368,6 @@ public class AstroSkyCalc {
    * @return dec hours in LST
    */
   public static double jd2lst(final double jd) {
-    return jd * AstroSkyCalc.LST_TO_JD;
+    return jd * LST_TO_JD;
   }
 }
