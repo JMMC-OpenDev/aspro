e56ee86f4f2179a85e967d384ff2948bf494cc03
bourgesl <bourges.laurent@gmail.com>
2011-02-02 17:40:36 +0000

    added observation for debugging purposes

--- src/fr/jmmc/aspro/model/event/WarningContainerEvent.java
+++ src/fr/jmmc/aspro/model/event/WarningContainerEvent.java
@@ -1,15 +1,19 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: WarningContainerEvent.java,v 1.1 2011-01-31 15:26:31 bourgesl Exp $"
+ * "@(#) $Id: WarningContainerEvent.java,v 1.2 2011-02-02 17:40:36 bourgesl Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2011/01/31 15:26:31  bourgesl
+ * new event to contain warnings
+ *
  */
 package fr.jmmc.aspro.model.event;
 
 import fr.jmmc.aspro.model.WarningContainer;
+import fr.jmmc.aspro.model.oi.ObservationSetting;
 
 /**
  * This event extends the ObservationEvent to contain only the warning container
@@ -21,10 +25,11 @@ public final class WarningContainerEvent extends ObservationEvent {
 
   /**
    * Public constructor
+   * @param observation observation related to this event
    * @param warningContainer warning container
    */
-  public WarningContainerEvent(final WarningContainer warningContainer) {
-    super(ObservationEventType.WARNINGS_READY, null);
+  public WarningContainerEvent(final ObservationSetting observation, final WarningContainer warningContainer) {
+    super(ObservationEventType.WARNINGS_READY, observation);
     this.warningContainer = warningContainer;
   }
 
