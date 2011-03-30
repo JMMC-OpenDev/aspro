bfbd88f05818cf27e756c298a09ada923e3044d4
bourgesl <bourges.laurent@gmail.com>
2011-03-30 09:03:22 +0000

    added MROI stations

--- src/asproGenInstConfigs.py
+++ src/asproGenInstConfigs.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 """ This script computes all tuples of stations for instruments with 2,3,4 channels """
 
+# CHARA:
 a=["S1","S2","E1","E2","W1","W2"]
+
+# MROI:
+#a = ["W0","W1","W2","W3","W4", "W5", "W6", "W7", "W8", "W9",
+# "N1","N2","N3","N4", "N5", "N6", "N7", "N8", "N9",
+# "S1","S2","S3","S4", "S5", "S6", "S7", "S8", "S9"
+# ]
+
 nbStations=len(a)
 
 print("<root>")
