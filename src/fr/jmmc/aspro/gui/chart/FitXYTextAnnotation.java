b2089e1a0fc0e1146876f2aa045b50923b14a883
bourgesl <bourges.laurent@gmail.com>
2011-04-08 15:11:41 +0000

    margin set to 1px

--- src/fr/jmmc/aspro/gui/chart/FitXYTextAnnotation.java
+++ src/fr/jmmc/aspro/gui/chart/FitXYTextAnnotation.java
@@ -1,11 +1,14 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: FitXYTextAnnotation.java,v 1.1 2010-10-21 16:47:43 bourgesl Exp $"
+ * "@(#) $Id: FitXYTextAnnotation.java,v 1.2 2011-04-08 15:11:41 bourgesl Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2010/10/21 16:47:43  bourgesl
+ * Custom XYTextAnnotation that adjust its font size to respect sizing constraints i.e. text width is fit to maxRadius
+ *
  */
 package fr.jmmc.aspro.gui.chart;
 
@@ -35,7 +38,7 @@ public final class FitXYTextAnnotation extends XYTextAnnotation
   /** minimum font size */
   private final static int MIN_SIZE_FONT = 5;
   /** margin in pixels */
-  private final static int MARGIN = 2;
+  private final static int MARGIN = 1;
 
   /* members */
   /** HACK : maximum width defined in data units to scale the text */
@@ -66,7 +69,7 @@ public final class FitXYTextAnnotation extends XYTextAnnotation
    * Set the maximum radius defined in data units to scale the text
    * @param maxRadius maximum radius defined in data units
    */
-  public void setMaxRadius(double maxRadius) {
+  public void setMaxRadius(final double maxRadius) {
     this.maxRadius = maxRadius;
   }
 
@@ -92,9 +95,6 @@ public final class FitXYTextAnnotation extends XYTextAnnotation
 
     // HACK (max radius) is related to bar width i.e. domain axis :
     final double j2MAX = domainAxis.lengthToJava2D(getMaxRadius(), dataArea, domainEdge) - 2d * MARGIN;
-
-//    System.out.println("text       = " + getText());
-//    System.out.println("j2MAX      = " + j2MAX);
 
     final Font bestFont = ChartUtils.autoFitText(getText(), j2MAX, g2, MIN_SIZE_FONT, MAX_SIZE_FONT);
 
