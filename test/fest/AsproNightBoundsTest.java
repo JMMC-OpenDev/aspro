30776e9fa7653962bc7f84c6dcf7276a7086d07f
bourgesl <bourges.laurent@gmail.com>
2011-04-22 15:33:34 +0000

    new test cases relative to observability date handling (lst / midnight)

---
+++ test/fest/AsproNightBoundsTest.java
@@ -0,0 +1,209 @@
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: AsproNightBoundsTest.java,v 1.1 2011-04-22 15:33:34 bourgesl Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ * Revision 1.11  2011/03/30 09:00:56  bourgesl
+ * use testgui module for Swing tests
+ *
+ * Revision 1.10  2011/03/17 15:58:32  bourgesl
+ * disable dev LAF menu
+ *
+ * Revision 1.9  2011/03/17 15:42:40  bourgesl
+ * added shouldOpenSampleMultiConf test
+ *
+ * Revision 1.8  2011/03/15 16:36:00  bourgesl
+ * comments
+ *
+ * Revision 1.7  2011/03/15 16:12:19  bourgesl
+ * disable statusBar updates when running fest swing tests
+ *
+ * Revision 1.6  2011/03/15 15:46:37  bourgesl
+ * new tests : PDF / OIFits / OB
+ * observability (detailed / baseline limits)
+ * Pops
+ *
+ * Revision 1.5  2011/03/14 17:43:10  bourgesl
+ * use setText instead of enterText
+ *
+ * Revision 1.4  2011/03/14 14:47:50  bourgesl
+ * added many tests
+ *
+ * Revision 1.3  2011/03/11 16:02:05  bourgesl
+ * updated scenario
+ *
+ * Revision 1.2  2011/03/11 15:04:07  bourgesl
+ * added image operations (resize, crop ...)
+ *
+ * Revision 1.1  2011/03/11 12:55:35  bourgesl
+ * added fest-swing test cases for Aspro 2
+ *
+ */
+package fest;
+
+import fest.common.JmcsApplicationSetup;
+
+import fest.common.JmcsFestSwingJUnitTestCase;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import javax.swing.JFormattedTextField;
+
+import org.fest.swing.annotation.GUITest;
+import org.fest.swing.edt.GuiActionRunner;
+import org.fest.swing.edt.GuiTask;
+import org.fest.swing.fixture.JPanelFixture;
+import org.fest.swing.fixture.JSpinnerFixture;
+
+import org.junit.Test;
+
+/**
+ * This simple tests changes the observation date to test night bounds
+ * 
+ * @author bourgesl
+ */
+public final class AsproNightBoundsTest extends JmcsFestSwingJUnitTestCase {
+
+  /** name of the tab pane corresponding to the observability panel */
+  private static final String TAB_OBSERVABILITY = "Observability";
+  /** number of days to traverse (30 years) */
+  private static final int DAYS = 10 * 365;
+
+  /**
+   * Define the application
+   */
+  static {
+    // disable dev LAF menu :
+    System.setProperty("jmcs.laf.menu", "false");
+
+    JmcsApplicationSetup.define(
+            fr.jmmc.aspro.AsproGui.class,
+            "-open", "/home/bourgesl/dev/aspro/test/Aspro2_sample.asprox");
+
+    // define robot delays :
+    defineRobotDelayBetweenEvents(SHORT_DELAY);
+
+    // define delay before taking screenshot :
+    defineScreenshotDelay(SHORT_DELAY);
+
+    // disable tooltips :
+    enableTooltips(false);
+
+    // customize PDF action to avoid use StatusBar :
+    fr.jmmc.aspro.gui.action.ExportPDFAction.setAvoidUseStatusBar(true);
+  }
+
+  /**
+   * Test if the application started correctly
+   */
+  @Test
+  @GUITest
+  public void shouldStart() {
+    window.textBox("starSearchField").setText("Simbad");
+
+    // waits for computation to finish :
+    AsproTestUtils.checkRunningTasks();
+
+    // select tab :
+    window.tabbedPane().selectTab(TAB_OBSERVABILITY);
+  }
+
+  /**
+   * Test observability plot when updating the observation date
+   */
+  @Test
+  @GUITest
+  public void shouldChangeDateForVLTI() {
+
+    final JPanelFixture form = window.panel("observationForm");
+
+    // select VLTI interferometer :
+    form.comboBox("jComboBoxInterferometer").selectItem("VLTI");
+
+    // waits for computation to finish :
+    AsproTestUtils.checkRunningTasks();
+
+    final JSpinnerFixture dateSpinner = form.spinner("jDateSpinner");
+
+    traverseDays(dateSpinner);
+  }
+
+  /**
+   * Test observability plot when updating the observation date
+   */
+  @Test
+  @GUITest
+  public void shouldChangeDateForCHARA() {
+
+    final JPanelFixture form = window.panel("observationForm");
+
+    // select CHARA interferometer :
+    form.comboBox("jComboBoxInterferometer").selectItem("CHARA");
+
+    // waits for computation to finish :
+    AsproTestUtils.checkRunningTasks();
+
+    // set PoPs to '54' :
+    final JFormattedTextField jTextPoPs = (JFormattedTextField) form.textBox("jTextPoPs").component();
+
+    GuiActionRunner.execute(new GuiTask() {
+
+      protected void executeInEDT() {
+        // Integer field :
+        jTextPoPs.setValue(Integer.valueOf(54));
+      }
+    });
+
+    // waits for computation to finish :
+    AsproTestUtils.checkRunningTasks();
+
+    final JSpinnerFixture dateSpinner = form.spinner("jDateSpinner");
+
+    traverseDays(dateSpinner);
+  }
+
+  /**
+   * Change the observation date starting from 01/01/1980 for n days where n = DAYS
+   * @param dateSpinner spinner field to update
+   */
+  private void traverseDays(final JSpinnerFixture dateSpinner) {
+    final int year = 2005;
+    final int month = 1;
+    final int day = 1;
+
+    final Calendar cal = new GregorianCalendar(year, month - 1, day);
+
+    dateSpinner.select(cal.getTime());
+
+    // waits for computation to finish :
+    AsproTestUtils.checkRunningTasks();
+
+    for (int i = 0; i < DAYS; i++) {
+
+      dateSpinner.increment();
+
+      // waits for computation to finish :
+      AsproTestUtils.checkRunningTasks();
+    }
+  }
+
+  /**
+   * Test the application exit sequence : ALWAYS THE LAST TEST
+   */
+  @Test
+  @GUITest
+  public void shouldExit() {
+    logger.severe("shouldExit test");
+
+    window.close();
+
+    confirmDialogDontSave();
+  }
+
+  /* 
+  --- Utility methods  ---------------------------------------------------------
+   */
+}
--- test/test/LSTTest.java
+++ test/test/LSTTest.java
@@ -1,33 +1,72 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: LSTTest.java,v 1.1 2010-10-01 15:21:09 bourgesl Exp $"
+ * "@(#) $Id: LSTTest.java,v 1.2 2011-04-22 15:33:34 bourgesl Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2010/10/01 15:21:09  bourgesl
+ * new test case for find LST0 bug (date lower > date upper)
+ *
  */
 package test;
 
+import edu.dartmouth.AstroAlmanac;
 import edu.dartmouth.AstroSkyCalc;
 import fr.jmmc.aspro.model.ConfigurationManager;
 import fr.jmmc.aspro.model.oi.InterferometerConfiguration;
 import fr.jmmc.aspro.model.oi.InterferometerDescription;
+import fr.jmmc.aspro.util.NumberUtils;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+import java.util.TimeZone;
 
 /**
- *
+ * This class tests the AstroSkyCalc class to locate LST [0;24] range for every day between 1/1/1980 and 31/12/2049
  * @author bourgesl
  */
-public class LSTTest {
+public final class LSTTest {
+
+  /** Class logger */
+  private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(LSTTest.class.getName());
+
+  /**
+   * Private constructor
+   */
+  private LSTTest() {
+    super();
+  }
 
   public static final void main(String[] args) {
+
+    // interferometer configuration in "CHARA" / "VLTI Period 88" :
+    final String interferometerConfiguration = /* "CHARA" */ /* */ "VLTI Period 88";
+
+    // indicates if the timestamps are expressed in LST or in UTC:
+    final boolean useLst = true;
+
+    // flag to enable the observability restriction due to the night:
+    final boolean useNightLimit = true;
+
+    // flag to center the plot arround midnight:
+    final boolean doCenterMidnight = true;
+
+
+
+
+    // Set the default locale to en-US locale (for Numerical Fields "." ",")
+    Locale.setDefault(Locale.US);
+
+    // Set the default timezone to GMT to handle properly the date in UTC :
+    TimeZone.setDefault(TimeZone.getTimeZone("GMT"));
 
     final ConfigurationManager cm = ConfigurationManager.getInstance();
 
 //    final InterferometerConfiguration intConf = cm.getInterferometerConfiguration("VLTI Period 87");
-    final InterferometerConfiguration intConf = cm.getInterferometerConfiguration("CHARA");
+    final InterferometerConfiguration intConf = cm.getInterferometerConfiguration(interferometerConfiguration);
 
     final InterferometerDescription interferometer = intConf.getInterferometer();
 
@@ -38,37 +77,126 @@ public class LSTTest {
     // define site :
     sc.defineSite(interferometer.getName(), interferometer.getPosSph());
 
+    // day expressed in milliseconds (depends on LST or JD day length) :
+    final double dayLength = ((useLst) ? 1d : AstroSkyCalc.LST_DAY_IN_JD) * 24d * 3600d * 1000d;
 
-    final Calendar cal = Calendar.getInstance();
-    cal.set(1980, 1, 1);
+    // 2 seconds of error (2 times rounding error on second field):
+    final double dayLenError = 2 * 1000d;
+
+//    logger.severe("dayLength = " + dayLength);
+
+    /*
+    final int year = 1980;
+    final int month = 10;
+    final int day = 1;
+     */
+    final int year = 1980;
+    final int month = 1;
+    final int day = 1;
+
+    final int lastYear = 2050;
+
+    final Calendar cal = new GregorianCalendar(year, month - 1, day);
     cal.setLenient(false);
+
+    double jdLower, jdUpper, jdMidnight;
+    Date dateMin, dateMax;
+    AstroAlmanac almanac;
+    double len;
 
     do {
 
-      System.out.println("date = " + cal.getTime());
+//      logger.severe("---------------------------------------\ndate = " + cal.getTime());
 
       // find the julian date corresponding to the LST origin LST=00:00:00 for the given date :
-      final double jdLst0 = sc.defineDate(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
+      jdLower = sc.defineDate(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH) + 1, cal.get(Calendar.DAY_OF_MONTH));
 
-      // warning : in LST, remove 1s to avoid 00:00:00 :
-      final double jdLst24 = sc.findJdForLst0(jdLst0 + 1d) - 1d / 86400d;
+      // check jdLst0 > jdUt12:
+      len = sc.getJdForLst0() - sc.getJdLt12();
+      if (len < 0d) {
+        throw new RuntimeException("jdLst0 [" + sc.getJdForLst0() + "]  is before jdLt12[" + sc.getJdLt12() + "]");
+      }
+      if (len > 1d) {
+        throw new RuntimeException("jdLst0 is next day [" + sc.getJdForLst0() + "]  - jdLt12[" + sc.getJdLt12() + "]");
+      }
 
-      final Date dateMin = sc.toDate(jdLst0, true);
-      final Date dateMax = sc.toDate(jdLst24, true);
+      // find the julian date corresponding to LST=00:00:00 next day:
+      jdUpper = sc.findJdForLst0(jdLower + AstroSkyCalc.LST_DAY_IN_JD);
 
-      final double len = dateMax.getTime() - dateMin.getTime();
+      dateMin = sc.toDate(jdLower, useLst);
+      dateMax = sc.toDate(jdUpper, useLst);
 
       if (dateMax.before(dateMin)) {
-        throw new RuntimeException("upper["+dateMax+"] before lower["+dateMin+"]");
+        throw new RuntimeException("upper[" + dateMax + "] before lower[" + dateMin + "]");
       }
-      if (len < 8.63E7) {
-        throw new RuntimeException("Invalid date length : upper["+dateMax+"] - lower["+dateMin+"]");
+      len = dateMax.getTime() - dateMin.getTime();
+
+      // 1s error:
+      if (!NumberUtils.equals(len, dayLength, dayLenError)) {
+        throw new RuntimeException("Invalid date length : upper[" + dateMax + "] - lower[" + dateMin + "] = " + len);
       }
 
+      /*
+      // find the julian date corresponding to LST=00:00:00 previous day:
+      final double jdLower1D = sc.findJdForLst0(jdLower - AstroSkyCalc.LST_DAY_IN_JD);
+
+      final Date dateMin1D = sc.toDate(jdLower1D, useLst);
+
+      logger.severe("date min-1 = " + dateMin1D);
+
+      len = dateMin.getTime() - dateMin1D.getTime();
+
+      // 1s error:
+      if (!NumberUtils.equals(len, dayLength, dayLenError)) {
+      throw new RuntimeException("Invalid date length : upper[" + dateMin + "] - lower[" + dateMin1D + "] = " + len);
+      }
+       */
+
+      // 1 - Find the day / twlight / night zones :
+      if (useNightLimit) {
+        //  sun rise/set with twilight : see NightlyAlmanac
+        almanac = sc.getAlmanac();
+
+        if (doCenterMidnight) {
+          jdMidnight = sc.findMidnight(almanac);
+
+//          logger.severe("jdMidnight = " + jdMidnight);
+
+          // check jdLst0 > jdUt12:
+          if (jdMidnight < sc.getJdLt12()) {
+            throw new RuntimeException("jdMidnight [" + jdMidnight + "]  is before jdLt12[" + sc.getJdLt12() + "]");
+          }
+
+          // adjust the jd bounds :
+          jdLower = jdMidnight - AstroSkyCalc.lst2jd(12d);
+          jdUpper = jdMidnight + AstroSkyCalc.lst2jd(12d);
+
+//          logger.severe("jdLower = " + jdLower);
+//          logger.severe("jdUpper = " + jdUpper);
+
+          dateMin = sc.toDate(jdLower, useLst);
+          dateMax = sc.toDate(jdUpper, useLst);
+
+//          logger.severe("date min = " + dateMin);
+//          logger.severe("date max = " + dateMax);
+
+          if (dateMax.before(dateMin)) {
+            throw new RuntimeException("upper[" + dateMax + "] before lower[" + dateMin + "]");
+          }
+          len = dateMax.getTime() - dateMin.getTime();
+
+          // 1s error:
+          if (!NumberUtils.equals(len, dayLength, dayLenError)) {
+            throw new RuntimeException("Invalid date length : upper[" + dateMax + "] - lower[" + dateMin + "] = " + len);
+          }
+        }
+      }
+
+
+      // next day:
       cal.add(Calendar.DAY_OF_MONTH, 1);
 
-    } while (cal.get(Calendar.YEAR) < 2050);
+    } while (cal.get(Calendar.YEAR) < lastYear);
 
   }
-
 }
