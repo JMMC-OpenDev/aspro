0041c235f310be43610b604594dd9585482cb9fa
bourgesl <bourges.laurent@gmail.com>
2011-05-18 17:10:05 +0000

    use MessagePane.showConfirmSaveChanges()

--- src/fr/jmmc/aspro/gui/action/NewObservationAction.java
+++ src/fr/jmmc/aspro/gui/action/NewObservationAction.java
@@ -4,25 +4,25 @@
 package fr.jmmc.aspro.gui.action;
 
 import fr.jmmc.aspro.model.ObservationManager;
-import fr.jmmc.mcs.gui.App;
+import fr.jmmc.mcs.gui.MessagePane;
+import fr.jmmc.mcs.gui.MessagePane.ConfirmSaveChanges;
 import fr.jmmc.mcs.gui.StatusBar;
 import fr.jmmc.mcs.util.ActionRegistrar;
 import fr.jmmc.mcs.util.RegisteredAction;
 import java.awt.event.ActionEvent;
 import java.util.logging.Level;
 import javax.swing.AbstractAction;
-import javax.swing.JOptionPane;
 
 /**
  * New observation settings action
  * @author bourgesl
  */
-public class NewObservationAction extends RegisteredAction {
+public final class NewObservationAction extends RegisteredAction {
 
   /** default serial UID for Serializable interface */
   private static final long serialVersionUID = 1;
   /** Class name. This name is used to register to the ActionRegistrar */
-  public final static String className = "fr.jmmc.aspro.gui.action.NewObservationAction";
+  public final static String className = NewObservationAction.class.getName();
   /** Action name. This name is used to register to the ActionRegistrar */
   public final static String actionName = "newObservation";
   /** Class logger */
@@ -45,21 +45,25 @@ public class NewObservationAction extends RegisteredAction {
     }
 
     // Ask the user if he wants to save modifications
-    final Object[] options = {"Save", "Cancel", "Don't Save"};
-    final int result = JOptionPane.showOptionDialog(App.getFrame(),
-            "Do you want to save changes before creating a new observation ?\nIf you don't save, your changes will be lost.\n\n",
-            null, JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE, null, options,
-            options[0]);
+    final ConfirmSaveChanges result = MessagePane.showConfirmSaveChanges("creating a new observation");
 
-    // If the User clicked the "Save" button, save and go on
-    if (result == 0) {
-      final AbstractAction action = (SaveObservationAction) ActionRegistrar.getInstance().get(SaveObservationAction.className, SaveObservationAction.actionName);
+    // Handle user choice
+    switch (result) {
+      // If the user clicked the "Save" button, save and go on
+      case Save:
+        final AbstractAction action = (SaveObservationAction) ActionRegistrar.getInstance().get(SaveObservationAction.className, SaveObservationAction.actionName);
 
-      action.actionPerformed(null);
-    }
-    // If the user clicked the "Cancel" button, don't quit
-    if (result == 1) {
-      return;
+        action.actionPerformed(null);
+        break;
+
+      // If the user clicked the "Don't Save" button, go on
+      case Ignore:
+        break;
+
+      // If the user clicked the "Cancel" button or pressed 'esc' key, return
+      case Cancel:
+      default: // Any other case
+        return;
     }
 
     // If the user clicked the "Don't Save" button, go on
