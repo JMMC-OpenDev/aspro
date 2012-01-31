f69b6844445c303af2b888fdfcc73e3aeabe01e9
bourgesl <bourges.laurent@gmail.com>
2012-01-31 14:36:58 +0000

    added custom PaintScale using IndexedColorModel

---
+++ src/fr/jmmc/aspro/gui/chart/ColorModelPaintScale.java
@@ -0,0 +1,159 @@
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
+package fr.jmmc.aspro.gui.chart;
+
+import fr.jmmc.jmal.image.ImageUtils;
+import java.awt.Color;
+import java.awt.Paint;
+import java.awt.image.IndexColorModel;
+import java.io.Serializable;
+
+import org.jfree.chart.HashUtilities;
+import org.jfree.chart.renderer.PaintScale;
+import org.jfree.util.PublicCloneable;
+
+/**
+ * This custom Paint scale returns colors using linear mapping defined by an indexed color model:
+ * see ImageUtils.
+ */
+public class ColorModelPaintScale
+        implements PaintScale, PublicCloneable, Serializable {
+
+  /** default serial UID for Serializable interface */
+  private static final long serialVersionUID = 1L;
+  /** The lower bound. */
+  private final double lowerBound;
+  /** The upper bound. */
+  private final double upperBound;
+  /** color model (lut) */
+  private transient final IndexColorModel colorModel;
+  /* internal */
+  /** index of the highest color */
+  private final int iMaxColor;
+  /** data to color linear scaling factor */
+  private final float scalingFactor;
+
+  /**
+   * Creates a new paint scale for values in the specified range.
+   *
+   * @param lowerBound  the lower bound.
+   * @param upperBound  the upper bound.
+   * @param colorModel color model
+   *
+   * @throws IllegalArgumentException if <code>lowerBound</code> is not
+   *       less than <code>upperBound</code>.
+   */
+  public ColorModelPaintScale(final double lowerBound, final double upperBound, final IndexColorModel colorModel) {
+    if (lowerBound >= upperBound) {
+      throw new IllegalArgumentException(
+              "Requires lowerBound < upperBound.");
+    }
+    this.lowerBound = lowerBound;
+    this.upperBound = upperBound;
+    this.colorModel = colorModel;
+
+    // prepare other variables:
+    this.iMaxColor = colorModel.getMapSize() - 1;
+    this.scalingFactor = ImageUtils.computeScalingFactor((float) this.lowerBound, (float) this.upperBound, colorModel.getMapSize());
+  }
+
+  /**
+   * Returns the lower bound.
+   *
+   * @return The lower bound.
+   *
+   * @see #getUpperBound()
+   */
+  @Override
+  public double getLowerBound() {
+    return this.lowerBound;
+  }
+
+  /**
+   * Returns the upper bound.
+   *
+   * @return The upper bound.
+   *
+   * @see #getLowerBound()
+   */
+  @Override
+  public double getUpperBound() {
+    return this.upperBound;
+  }
+
+  /**
+   * Returns a paint for the specified value.
+   *
+   * @param value  the value (must be within the range specified by the
+   *         lower and upper bounds for the scale).
+   *
+   * @return A paint for the specified value.
+   */
+  @Override
+  public Paint getPaint(final double value) {
+    final int colorIdx = ImageUtils.getColor(this.colorModel, this.iMaxColor, (float) ((value - this.lowerBound) * this.scalingFactor));
+    return new Color(this.colorModel.getRGB(colorIdx));
+  }
+
+  /**
+   * Tests this <code>ColorModelPaintScale</code> instance for equality with an
+   * arbitrary object.  This method returns <code>true</code> if and only
+   * if:
+   * <ul>
+   * <li><code>obj</code> is not <code>null</code>;</li>
+   * <li><code>obj</code> is an instance of <code>ColorModelPaintScale</code>;</li>
+   * </ul>
+   *
+   * @param obj  the object (<code>null</code> permitted).
+   *
+   * @return A boolean.
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == this) {
+      return true;
+    }
+    if (!(obj instanceof ColorModelPaintScale)) {
+      return false;
+    }
+    ColorModelPaintScale that = (ColorModelPaintScale) obj;
+    if (this.lowerBound != that.getLowerBound()) {
+      return false;
+    }
+    if (this.upperBound != that.getUpperBound()) {
+      return false;
+    }
+    if (!this.colorModel.equals(that.colorModel)) {
+      return false;
+    }
+    return true;
+  }
+
+  /**
+   * Returns a hash code for this instance.
+   *
+   * @return A hash code.
+   */
+  @Override
+  public int hashCode() {
+    int hash = 7;
+    hash = HashUtilities.hashCode(hash, this.lowerBound);
+    hash = HashUtilities.hashCode(hash, this.upperBound);
+    hash = 43 * hash;
+    return hash;
+  }
+
+  /**
+   * Returns a clone of this <code>ColorModelPaintScale</code> instance.
+   *
+   * @return A clone.
+   *
+   * @throws CloneNotSupportedException if there is a problem cloning this
+   *     instance.
+   */
+  @Override
+  public Object clone() throws CloneNotSupportedException {
+    return super.clone();
+  }
+}
