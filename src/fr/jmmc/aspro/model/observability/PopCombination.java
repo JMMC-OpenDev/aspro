901c3ec1c032797a2923c45ecda87bca4d7ca42d
bourgesl <bourges.laurent@gmail.com>
2012-12-18 16:18:24 +0000

    use SharedPopCombination (cached) to reduce memory footprint (avoid computing tuples)

--- src/fr/jmmc/aspro/model/observability/PopCombination.java
+++ src/fr/jmmc/aspro/model/observability/PopCombination.java
@@ -4,7 +4,6 @@
 package fr.jmmc.aspro.model.observability;
 
 import fr.jmmc.aspro.model.oi.Pop;
-import java.util.List;
 
 /**
  * This class defines a Pop combination associated to a complete baseline (i.e. for a given station list)
@@ -13,34 +12,17 @@ import java.util.List;
  */
 public final class PopCombination {
 
-  /** identifier of this combination */
-  private final String identifier;
-  /** list of Pops with the same order than the station list */
-  private final List<Pop> popList;
-  /** list of pop delays (offset) corresponding to each base line (2 telescopes) */
+  /** shared pop combination */
+  private final SharedPopCombination popComb;
+  /** array of pop delays (offset) corresponding to each base line (2 telescopes) */
   private double[] popOffsets;
 
   /**
-   * Factory method : create one PopCombination for the given list of PoPs
-   * @param popList list of PoPs
-   * @param sb string builder used to build the identifier (empty when returning from this method)
-   * @return one PopCombination for the given list of PoPs
+   * Constructor with the given shared Pop combination
+   * @param popComb shared pop combination
    */
-  public static PopCombination newInstance(final List<Pop> popList, final StringBuilder sb) {
-    final String id = Pop.toString(sb, popList);
-    // recycle the given string builder
-    sb.setLength(0);
-    return new PopCombination(id, popList);
-  }
-
-  /**
-   * Constructor with the given list of Pops
-   * @param identifier identifier of this combination
-   * @param popList list of Pops
-   */
-  private PopCombination(final String identifier, final List<Pop> popList) {
-    this.popList = popList;
-    this.identifier = identifier;
+  public PopCombination(final SharedPopCombination popComb) {
+    this.popComb = popComb;
   }
 
   /**
@@ -48,28 +30,28 @@ public final class PopCombination {
    * @return identifier of this combination
    */
   public String getIdentifier() {
-    return identifier;
+    return this.popComb.getIdentifier();
   }
 
   /**
-   * Return the list of Pops
-   * @return list of Pops
+   * Return the array of Pops
+   * @return array of Pops
    */
-  public List<Pop> getPopList() {
-    return popList;
+  public Pop[] getPops() {
+    return this.popComb.getPops();
   }
 
   /**
-   * Return the list of pop delays per baseline
-   * @return list of pop delays per baseline
+   * Return the array of pop delays per baseline
+   * @return array of pop delays per baseline
    */
   public double[] getPopOffsets() {
     return popOffsets;
   }
 
   /**
-   * Define the list of pop delays per baseline
-   * @param popOffsets list of pop delays per baseline
+   * Define the array of pop delays per baseline
+   * @param popOffsets array of pop delays per baseline
    */
   public void setPopOffsets(final double[] popOffsets) {
     this.popOffsets = popOffsets;
@@ -81,7 +63,7 @@ public final class PopCombination {
    */
   @Override
   public String toString() {
-    return this.identifier;
+    return getIdentifier();
   }
 
   /**
@@ -90,7 +72,7 @@ public final class PopCombination {
    */
   public void toString(final StringBuilder sb) {
     sb.append(" + ");
-    for (Pop pop : getPopList()) {
+    for (Pop pop : getPops()) {
       sb.append(pop.getName()).append(' ');
     }
   }
