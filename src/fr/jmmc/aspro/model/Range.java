c791265db99ac3d7b9896f4a94ac129c2d49fdbe
bourgesl <bourges.laurent@gmail.com>
2012-12-20 15:32:54 +0000

    use customized BinarySort from Arrays.sort() to avoid array copies on small arrays (< 64)

--- src/fr/jmmc/aspro/model/Range.java
+++ src/fr/jmmc/aspro/model/Range.java
@@ -5,7 +5,6 @@ package fr.jmmc.aspro.model;
 
 import fr.jmmc.jmcs.util.ObjectUtils;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -313,10 +312,11 @@ public final class Range {
    * to consider the point as valid
    * @param ranges list of ranges to merge
    * @param nValid number of ranges to consider a point is valid
+   * @param rangeFactory Factory used to create Range instances
    * @return new list of ranges
    */
-  public static List<Range> intersectRanges(final List<Range> ranges, final int nValid) {
-    return intersectRanges(ranges, nValid, null);
+  public static List<Range> intersectRanges(final List<Range> ranges, final int nValid, final RangeFactory rangeFactory) {
+    return intersectRanges(ranges, nValid, null, rangeFactory);
   }
 
   /**
@@ -325,9 +325,11 @@ public final class Range {
    * @param ranges list of ranges to merge
    * @param nValid number of ranges to consider a point is valid
    * @param results output list of ranges
+   * @param rangeFactory Factory used to create Range instances
    * @return new list of ranges or null
    */
-  public static List<Range> intersectRanges(final List<Range> ranges, final int nValid, final List<Range> results) {
+  public static List<Range> intersectRanges(final List<Range> ranges, final int nValid,
+          final List<Range> results, final RangeFactory rangeFactory) {
     final int len = ranges.size() * 2;
 
     // table of start/end time :
@@ -339,7 +341,7 @@ public final class Range {
       limits[n++] = new RangeLimit(range.getMax(), false);
     }
 
-    return intersectRanges(limits, n, nValid, results);
+    return intersectRanges(limits, n, nValid, results, rangeFactory);
   }
 
   /**
@@ -349,14 +351,17 @@ public final class Range {
    * @param nLimits number of range limits present in the given array
    * @param nValid number of ranges to consider a point is valid
    * @param results output list of ranges
+   * @param rangeFactory Factory used to create Range instances
    * @return new list of ranges or null
    */
-  public static List<Range> intersectRanges(final RangeLimit[] limits, final int nLimits, final int nValid, final List<Range> results) {
+  public static List<Range> intersectRanges(final RangeLimit[] limits, final int nLimits, final int nValid,
+          final List<Range> results, final RangeFactory rangeFactory) {
 
-    // sort the array by increasing time :
-    Arrays.sort(limits, 0, nLimits);
+    // use customized binary sort to be in-place (no memory usage):
+    // inspired from Arrays.sort but do not use tim sort as it makes array copies:
+    binarySort(limits, 0, nLimits, countRunAndMakeAscending(limits, 0, nLimits));
 
-    //  Explore range. When the running sum of flag is equal to the
+    //  Explore range: when the running sum of flag is equal to the
     //  number nValid, we are in a valid range
 
     List<Range> mRanges = results;
@@ -375,10 +380,144 @@ public final class Range {
           // lazy instanciation (statically 1 range only) :
           mRanges = new ArrayList<Range>(1);
         }
-        mRanges.add(new Range(limits[i].position, limits[i + 1].position));
+        mRanges.add(rangeFactory.valueOf(limits[i].position, limits[i + 1].position));
       }
     }
 
     return mRanges;
+  }
+
+  // --- Customized BinarySort from Arrays.sort() -----------------------------
+  /**
+   * Sorts the specified portion of the specified array using a binary
+   * insertion sort.  This is the best method for sorting small numbers
+   * of elements.  It requires O(n log n) compares, but O(n^2) data
+   * movement (worst case).
+   *
+   * If the initial part of the specified range is already sorted,
+   * this method can take advantage of it: the method assumes that the
+   * elements from index {@code lo}, inclusive, to {@code start},
+   * exclusive are already sorted.
+   *
+   * @param a the array in which a range is to be sorted
+   * @param lo the index of the first element in the range to be sorted
+   * @param hi the index after the last element in the range to be sorted
+   * @param start the index of the first element in the range that is
+   *        not already known to be sorted ({@code lo <= start <= hi})
+   */
+  @SuppressWarnings("unchecked")
+  private static void binarySort(final Comparable[] a, final int lo, final int hi, int start) {
+    if (start == lo) {
+      start++;
+    }
+    int left, right, mid, n;
+    Comparable pivot;
+
+    for (; start < hi; start++) {
+      pivot = a[start];
+
+      // Set left (and right) to the index where a[start] (pivot) belongs
+      left = lo;
+      right = start;
+      /*
+       * Invariants:
+       *   pivot >= all in [lo, left).
+       *   pivot <  all in [right, start).
+       */
+      while (left < right) {
+        mid = (left + right) >>> 1;
+        if (pivot.compareTo(a[mid]) < 0) {
+          right = mid;
+        } else {
+          left = mid + 1;
+        }
+      }
+
+      /*
+       * The invariants still hold: pivot >= all in [lo, left) and
+       * pivot < all in [left, start), so pivot belongs at left.  Note
+       * that if there are elements equal to pivot, left points to the
+       * first slot after them -- that's why this sort is stable.
+       * Slide elements over to make room for pivot.
+       */
+      n = start - left;  // The number of elements to move
+      // Switch is just an optimization for arraycopy in default case
+      switch (n) {
+        case 2:
+          a[left + 2] = a[left + 1];
+        case 1:
+          a[left + 1] = a[left];
+          break;
+        default:
+          System.arraycopy(a, left, a, left + 1, n);
+      }
+      a[left] = pivot;
+    }
+  }
+
+  /**
+   * Returns the length of the run beginning at the specified position in
+   * the specified array and reverses the run if it is descending (ensuring
+   * that the run will always be ascending when the method returns).
+   *
+   * A run is the longest ascending sequence with:
+   *
+   *    a[lo] <= a[lo + 1] <= a[lo + 2] <= ...
+   *
+   * or the longest descending sequence with:
+   *
+   *    a[lo] >  a[lo + 1] >  a[lo + 2] >  ...
+   *
+   * For its intended use in a stable mergesort, the strictness of the
+   * definition of "descending" is needed so that the call can safely
+   * reverse a descending sequence without violating stability.
+   *
+   * @param a the array in which a run is to be counted and possibly reversed
+   * @param lo index of the first element in the run
+   * @param hi index after the last element that may be contained in the run.
+   It is required that {@code lo < hi}.
+   * @return  the length of the run beginning at the specified position in
+   *          the specified array
+   */
+  @SuppressWarnings("unchecked")
+  private static int countRunAndMakeAscending(final Comparable[] a, final int lo, final int hi) {
+    int runHi = lo + 1;
+    if (runHi == hi) {
+      return 1;
+    }
+
+    // Find end of run, and reverse range if descending
+    if (a[runHi++].compareTo(a[lo]) < 0) {
+      // Descending
+      while (runHi < hi && a[runHi].compareTo(a[runHi - 1]) < 0) {
+        runHi++;
+      }
+      reverseRange(a, lo, runHi);
+    } else {
+      // Ascending
+      while (runHi < hi && a[runHi].compareTo(a[runHi - 1]) >= 0) {
+        runHi++;
+      }
+    }
+
+    return runHi - lo;
+  }
+
+  /**
+   * Reverse the specified range of the specified array.
+   *
+   * @param a the array in which a range is to be reversed
+   * @param lo the index of the first element in the range to be reversed
+   * @param hi the index after the last element in the range to be reversed
+   */
+  @SuppressWarnings("unchecked")
+  private static void reverseRange(final Comparable[] a, int lo, int hi) {
+    hi--;
+    Comparable<?> t;
+    while (lo < hi) {
+      t = a[lo];
+      a[lo++] = a[hi];
+      a[hi--] = t;
+    }
   }
 }
