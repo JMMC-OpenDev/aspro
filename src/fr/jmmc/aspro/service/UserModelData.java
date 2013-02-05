4d1c0b85470a26b527b95cf8a5e53286782481a3
bourgesl <bourges.laurent@gmail.com>
2013-02-05 09:49:35 +0000

    added getWavelength() and getIncWL()

--- src/fr/jmmc/aspro/service/UserModelData.java
+++ src/fr/jmmc/aspro/service/UserModelData.java
@@ -12,64 +12,88 @@ import fr.jmmc.oitools.image.FitsImage;
  */
 public final class UserModelData extends OIBase {
 
-  /* members */
-  /** FFT ready fits image */
-  private FitsImage fitsImage = null;
-  /** 
-   * Flattened data points (1D) stored with col and row: [data col row] 
-   * spatial coordinates along the column and row axis (rad)
-   */
-  private float[] data1D = null;
+    /* members */
+    /** FFT ready fits image */
+    private FitsImage fitsImage = null;
+    /** 
+     * Flattened data points (1D) stored with col and row: [data col row] 
+     * spatial coordinates along the column and row axis (rad)
+     */
+    private float[] data1D = null;
 
-  /**
-   * Public constructor
-   */
-  public UserModelData() {
-    super();
-  }
+    /**
+     * Public constructor
+     */
+    public UserModelData() {
+        super();
+    }
 
-  /**
-   * Return the FFT ready fits image
-   * @return FFT ready fits image
-   */
-  public FitsImage getFitsImage() {
-    return fitsImage;
-  }
+    /**
+     * Return the FFT ready fits image
+     * @return FFT ready fits image
+     */
+    public FitsImage getFitsImage() {
+        return fitsImage;
+    }
 
-  /**
-   * Define the FFT ready fits image
-   * @param fitsImage FFT ready fits image
-   */
-  public void setFitsImage(final FitsImage fitsImage) {
-    this.fitsImage = fitsImage;
-  }
+    /**
+     * Return the image wavelength in meters
+     * @return image wavelength in meters or Double.NaN if undefined
+     */
+    public Double getWaveLength() {
+        return fitsImage.getWaveLength();
+    }
 
-  /**
-   * Define model data
-   * @param data1D flattened data points (1D)
-   * @return this instance
-   */
-  public UserModelData set(final float[] data1D) {
-    this.data1D = data1D;
-    return this;
-  }
+    /**
+     * Return the wavelength increment along the wavelength axis (meter per pixel)
+     * @return wavelength increment along the wavelength axis (meter per pixel)
+     */
+    public double getIncWL() {
+        return fitsImage.getIncWL();
+    }
 
-  /**
-   * Return the number of data points
-   * 
-   * TODO: x 3 or / 3: decide soon
-   * 
-   * @return number of data points
-   */
-  public int getNData() {
-    return this.data1D.length;
-  }
+    /**
+     * Define the FFT ready fits image
+     * @param fitsImage FFT ready fits image
+     */
+    public void setFitsImage(final FitsImage fitsImage) {
+        this.fitsImage = fitsImage;
+    }
 
-  /**
-   * Return the flattened data points (1D) [data col row]
-   * @return flattened data points (1D) [data col row]
-   */
-  public float[] getData1D() {
-    return data1D;
-  }
+    /**
+     * Define model data
+     * @param data1D flattened data points (1D)
+     * @return this instance
+     */
+    public UserModelData set(final float[] data1D) {
+        this.data1D = data1D;
+        return this;
+    }
+
+    /**
+     * Return the number of data points
+     * 
+     * TODO: x 3 or / 3: decide soon
+     * 
+     * @return number of data points
+     */
+    public int getNData() {
+        return this.data1D.length;
+    }
+
+    /**
+     * Return the flattened data points (1D) [data col row]
+     * @return flattened data points (1D) [data col row]
+     */
+    public float[] getData1D() {
+        return data1D;
+    }
+
+    /**
+     * @return string representation from FitsImage information
+     */
+    @Override
+    public String toString() {
+        return "UserModelData:" + getFitsImage() + " - nData=" + getNData();
+    }
 }
