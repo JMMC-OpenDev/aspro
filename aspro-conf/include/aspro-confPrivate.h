ea068094144542959c085bc979ef3a625095cbaa
bourgesl <bourges.laurent@gmail.com>
2013-03-29 08:42:48 +0000

    Automatically tagged ASPRO2-conf in version ASPRO2-CONF_2013_314 from trunk  (mkfRepoUtil)

---
+++ aspro-conf/include/aspro-confPrivate.h
@@ -0,0 +1,24 @@
+#ifndef aspro-confPrivate_H
+#define aspro-confPrivate_H
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
+
+/**
+ * @file
+ * aspro-conf private header file.
+ */
+
+
+/*
+ * Constants definition
+ */
+
+/* Module name */
+define MODULE_ID "aspro-conf"
+
+ 
+
+#endif /*!aspro-confPrivate_H*/
+
+/*___oOo___*/
