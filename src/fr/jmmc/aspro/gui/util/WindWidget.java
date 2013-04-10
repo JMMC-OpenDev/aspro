58ebbd0caae0e4b712eb089fc7bec7aff17e7e76
bourgesl <bourges.laurent@gmail.com>
2013-04-10 11:48:08 +0000

    upgrade to Java 6 (compass is always available)

--- src/fr/jmmc/aspro/gui/util/WindWidget.java
+++ src/fr/jmmc/aspro/gui/util/WindWidget.java
@@ -3,8 +3,6 @@
  ******************************************************************************/
 package fr.jmmc.aspro.gui.util;
 
-import fr.jmmc.jmcs.gui.util.SwingUtils;
-import fr.jmmc.jmcs.util.NumberUtils;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Point;
@@ -12,14 +10,8 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
-import java.util.Hashtable;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JSlider;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import net.jafama.FastMath;
-import org.apache.commons.lang.SystemUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -42,15 +34,7 @@ public abstract class WindWidget extends JPanel {
      * @return new Wind widget (azimuth)
      */
     public static WindWidget create() {
-        // Compass widget requires Java 6:
-        if (SystemUtils.JAVA_VERSION_FLOAT >= 1.6f) {
-            try {
-                return new WindCompassWidget();
-            } catch (Throwable th) {
-                logger.warn("Unable to create WindCompassWidget:", th);
-            }
-        }
-        return new WindSliderWidget();
+        return new WindCompassWidget();
     }
 
     /**
@@ -99,8 +83,7 @@ public abstract class WindWidget extends JPanel {
         /** store value to fix Compass.getValue() implementation */
         private double azValue = 0d;
         /** compass */
-//        private eu.hansolo.steelseries.extras.Compass compass = null;        
-        private fr.jmmc.aspro.gui.util.CustomCompass compass = null;
+        private CustomCompass compass = null;
 
         /**
          * Constructor
@@ -151,8 +134,7 @@ public abstract class WindWidget extends JPanel {
 
         @Override
         protected void initComponents() {
-//            this.compass = new eu.hansolo.steelseries.extras.Compass();
-            this.compass = new fr.jmmc.aspro.gui.util.CustomCompass();
+            this.compass = new CustomCompass();
             this.compass.setNiceScale(true);
             this.compass.setThreshold(5d);
             this.compass.setMinimumSize(new Dimension(16, 16));
@@ -183,101 +165,6 @@ public abstract class WindWidget extends JPanel {
             });
 
             this.add(this.compass, BorderLayout.CENTER);
-        }
-    }
-
-    /**
-     * Slider implementation
-     */
-    private static final class WindSliderWidget extends WindWidget implements ChangeListener {
-
-        /** default serial UID for Serializable interface */
-        private static final long serialVersionUID = 1;
-        /** compass */
-        private JSlider slider = null;
-
-        /**
-         * Constructor
-         */
-        private WindSliderWidget() {
-            setName("WindWidget");
-            initComponents();
-
-            // update tooltip with init value
-            this.slider.setToolTipText(getWindDescription(getValue()));
-        }
-
-        /**
-         * Override setEnabled to keep panel enabled but not the compass widget
-         * @param enabled
-         */
-        @Override
-        public void setEnabled(final boolean enabled) {
-            this.slider.setEnabled(enabled);
-            super.setEnabled(true);
-        }
-
-        @Override
-        public double getValue() {
-            return this.slider.getValue();
-        }
-
-        @Override
-        public void setValue(final double value) {
-            final int oldValue = (int) getValue();
-            final int newValue = (int) (Math.round(value / 5d)) * 5;
-
-            if (newValue != oldValue) {
-                this.slider.setValue(newValue);
-
-                if (logger.isDebugEnabled()) {
-                    logger.debug("slider: {}", value);
-                }
-
-                this.firePropertyChange(PROPERTY_VALUE, oldValue, newValue);
-            }
-
-            // update tooltip
-            this.slider.setToolTipText(getWindDescription(value));
-        }
-
-        @Override
-        protected void initComponents() {
-            this.slider = new JSlider(0, 359, 0);
-            this.slider.setMajorTickSpacing(45);
-            this.slider.setPaintTicks(true);
-            // reduce widget size:
-            this.slider.setPreferredSize(new Dimension(40, 40));
-
-            // Create the label table:
-            final Hashtable<Integer, JLabel> labelTable = new Hashtable<Integer, JLabel>(4);
-            labelTable.put(NumberUtils.valueOf(0), new JLabel("N"));
-            labelTable.put(NumberUtils.valueOf(90), new JLabel("E"));
-            labelTable.put(NumberUtils.valueOf(180), new JLabel("S"));
-            labelTable.put(NumberUtils.valueOf(270), new JLabel("W"));
-            this.slider.setLabelTable(labelTable);
-
-            this.slider.setPaintLabels(true);
-            this.slider.addChangeListener(this);
-
-            this.add(this.slider, BorderLayout.CENTER);
-        }
-
-        /**
-         * Handle the stateChanged event from the slider.
-         * @param ce slider change event
-         */
-        @Override
-        public void stateChanged(final ChangeEvent ce) {
-            if (!this.slider.getValueIsAdjusting()) {
-
-                SwingUtils.invokeLaterEDT(new Runnable() {
-                    @Override
-                    public void run() {
-                        setValue(slider.getValue());
-                    }
-                });
-            }
         }
     }
 }
