f68d38157733ddbbabc7fc7f76c8504ed8a4c8b2
bourgesl <bourges.laurent@gmail.com>
2013-05-02 15:58:40 +0000

    code formatting

--- src/fr/jmmc/aspro/gui/chart/FitXYTextAnnotation.java
+++ src/fr/jmmc/aspro/gui/chart/FitXYTextAnnotation.java
@@ -22,66 +22,66 @@ import org.jfree.util.PublicCloneable;
 public final class FitXYTextAnnotation extends XYTextAnnotation
         implements Cloneable, PublicCloneable, Serializable {
 
-  /** For serialization. */
-  private static final long serialVersionUID = 1L;
-  /** margin in pixels */
-  private final static int MARGIN = 1;
+    /** For serialization. */
+    private static final long serialVersionUID = 1L;
+    /** margin in pixels */
+    private final static double MARGIN = 1d;
 
-  /**
-   * Creates a new annotation to be displayed at the given coordinates.  The
-   * coordinates are specified in data space (they will be converted to
-   * Java2D space for display).
-   *
-   * @param text  the text (<code>null</code> not permitted).
-   * @param x  the x-coordinate (in data space).
-   * @param y  the y-coordinate (in data space).
-   */
-  public FitXYTextAnnotation(final String text, final double x, final double y) {
-    super(text, x, y);
-  }
-
-  /**
-   * Draws the annotation.
-   *
-   * @param g2d  the graphics device.
-   * @param plot  the plot.
-   * @param dataArea  the data area.
-   * @param domainAxis  the domain axis.
-   * @param rangeAxis  the range axis.
-   * @param rendererIndex  the renderer index.
-   * @param info  an optional info object that will be populated with
-   *              entity information.
-   */
-  @Override
-  public void draw(final Graphics2D g2d, final XYPlot plot, final Rectangle2D dataArea,
-                   final ValueAxis domainAxis, final ValueAxis rangeAxis,
-                   final int rendererIndex, final PlotRenderingInfo info) {
-
-    // Use Observability Plot Context to determine once for all the appropriate font size
-    // that best fits the bar width; if too large, do not render the annotation
-
-    final ObservabilityPlotContext renderContext = ObservabilityPlotContext.getInstance();
-
-    final Font bestFont;
-
-    if (renderContext.autoFitTimeWidthDone()) {
-      bestFont = renderContext.autoFitTimeWidthFont();
-    } else {
-      // first time, perform fit:
-      final RectangleEdge domainEdge = Plot.resolveDomainAxisLocation(plot.getDomainAxisLocation(), plot.getOrientation());
-
-      // convert max text width in data units (equals to bar width) i.e. domain axis :
-      final double j2Max = domainAxis.lengthToJava2D(renderContext.getMaxTextWidth(), dataArea, domainEdge) - 2d * MARGIN;
-
-      bestFont = renderContext.autoFitTimeWidthFont(g2d, j2Max);
+    /**
+     * Creates a new annotation to be displayed at the given coordinates.  The
+     * coordinates are specified in data space (they will be converted to
+     * Java2D space for display).
+     *
+     * @param text  the text (<code>null</code> not permitted).
+     * @param x  the x-coordinate (in data space).
+     * @param y  the y-coordinate (in data space).
+     */
+    public FitXYTextAnnotation(final String text, final double x, final double y) {
+        super(text, x, y);
     }
 
-    // Dont render if the text do not fit in block size:
-    if (bestFont == null) {
-      return;
-    }
-    setFont(bestFont);
+    /**
+     * Draws the annotation.
+     *
+     * @param g2d  the graphics device.
+     * @param plot  the plot.
+     * @param dataArea  the data area.
+     * @param domainAxis  the domain axis.
+     * @param rangeAxis  the range axis.
+     * @param rendererIndex  the renderer index.
+     * @param info  an optional info object that will be populated with
+     *              entity information.
+     */
+    @Override
+    public void draw(final Graphics2D g2d, final XYPlot plot, final Rectangle2D dataArea,
+            final ValueAxis domainAxis, final ValueAxis rangeAxis,
+            final int rendererIndex, final PlotRenderingInfo info) {
 
-    super.draw(g2d, plot, dataArea, domainAxis, rangeAxis, rendererIndex, info);
-  }
+        // Use Observability Plot Context to determine once for all the appropriate font size
+        // that best fits the bar width; if too large, do not render the annotation
+
+        final ObservabilityPlotContext renderContext = ObservabilityPlotContext.getInstance();
+
+        final Font bestFont;
+
+        if (renderContext.autoFitTimeWidthDone()) {
+            bestFont = renderContext.autoFitTimeWidthFont();
+        } else {
+            // first time, perform fit:
+            final RectangleEdge domainEdge = Plot.resolveDomainAxisLocation(plot.getDomainAxisLocation(), plot.getOrientation());
+
+            // convert max text width in data units (equals to bar width) i.e. domain axis :
+            final double j2Max = domainAxis.lengthToJava2D(renderContext.getMaxTextWidth(), dataArea, domainEdge) - 2d * MARGIN;
+
+            bestFont = renderContext.autoFitTimeWidthFont(g2d, j2Max);
+        }
+
+        // Dont render if the text do not fit in block size:
+        if (bestFont == null) {
+            return;
+        }
+        setFont(bestFont);
+
+        super.draw(g2d, plot, dataArea, domainAxis, rangeAxis, rendererIndex, info);
+    }
 }
