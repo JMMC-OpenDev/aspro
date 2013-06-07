9d4984948419c4326ee8bfb6c6a3cb5db59aec98
bourgesl <bourges.laurent@gmail.com>
2013-06-07 16:04:43 +0000

    added time (LST or UTC) used by tooltips added computeUVPoints(ha) to compute time annotations fix obs ha range checks (min, max)

--- src/fr/jmmc/aspro/service/UVCoverageService.java
+++ src/fr/jmmc/aspro/service/UVCoverageService.java
@@ -13,7 +13,6 @@ import fr.jmmc.aspro.model.observability.StarData;
 import fr.jmmc.aspro.model.oi.FocalInstrumentMode;
 import fr.jmmc.aspro.model.oi.ObservationSetting;
 import fr.jmmc.aspro.model.oi.Target;
-import fr.jmmc.aspro.model.oi.TargetConfiguration;
 import fr.jmmc.aspro.model.uvcoverage.UVBaseLineData;
 import fr.jmmc.aspro.model.uvcoverage.UVCoverageData;
 import fr.jmmc.aspro.model.uvcoverage.UVRangeBaseLineData;
@@ -21,6 +20,7 @@ import fr.jmmc.aspro.service.UserModelService.MathMode;
 import fr.jmmc.aspro.util.AngleUtils;
 import fr.jmmc.aspro.util.TestUtils;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import net.jafama.DoubleWrapper;
 import net.jafama.FastMath;
@@ -84,10 +84,6 @@ public final class UVCoverageService {
     private double lambdaMax;
     /** number of spectral channels (used by OIFits) */
     private int nSpectralChannels;
-    /** HA min in decimal hours */
-    private double haMin = AsproConstants.HA_MIN;
-    /** HA max in decimal hours */
-    private double haMax = AsproConstants.HA_MAX;
 
     /* reused observability data */
     /** sky calc instance */
@@ -284,22 +280,6 @@ public final class UVCoverageService {
     }
 
     /**
-     * Check the given hour angle inside the [-haElev; haElev]
-     * @param ha ha to check
-     * @param haElev rise/set ha
-     * @return ha or -haElev or haElev
-     */
-    private static double checkHA(final double ha, final double haElev) {
-        if (ha < -haElev) {
-            return -haElev;
-        }
-        if (ha > haElev) {
-            return haElev;
-        }
-        return ha;
-    }
-
-    /**
      * Compute UV points (observable) inside HA min/max ranges
      */
     private void computeObservableUV() {
@@ -311,138 +291,151 @@ public final class UVCoverageService {
         }
 
         if (obsRangesHA != null) {
+
             // use observable HA bounds:
             final Double haLower = Range.getMinimum(obsRangesHA);
             final Double haUpper = Range.getMaximum(obsRangesHA);
+            
+            if (haLower != null && haUpper != null) {
 
-            if (logger.isDebugEnabled()) {
-                logger.debug("HA min/Max: {} - {}", haLower, haUpper);
-                logger.debug("HA ObsTime: {}", haObsTime);
-            }
+                final double haMin = haLower.doubleValue();
+                final double haMax = haUpper.doubleValue();
 
-            final double step = this.haStep;
+                if (logger.isDebugEnabled()) {
+                    logger.debug("HA min/Max: {} - {}", haMin, haMax);
+                    logger.debug("HA ObsTime: {}", haObsTime);
+                }
 
-            // estimate the number of HA points :
-            final int capacity = (int) Math.round((haUpper - haLower) / step) + 1;
+                final double precRA = this.starData.getPrecRA();
 
-            // First pass : find observable HA values :
+                final double step = this.haStep;
 
-            // use safety limit to avoid out of memory errors :
-            final double[] haValues = new double[(capacity > MAX_HA_POINTS) ? MAX_HA_POINTS : capacity];
+                // estimate the number of HA points :
+                final int capacity = (int) Math.round((haMax - haMin) / step) + 1;
 
-            Range obsRange;
-            int j = 0;
-            for (double ha = haLower; ha <= haUpper; ha += step) {
+                // First pass : find observable HA values :
 
-                // check HA start:
-                if ((obsRange = Range.find(obsRangesHA, ha, HA_PRECISION)) != null) {
-                    // check HA end:
-                    if (obsRange.contains(ha + haObsTime, HA_PRECISION)) {
-                        haValues[j] = ha;
-                        j++;
+                // use safety limit to avoid out of memory errors :
+                final int haLen = (capacity > MAX_HA_POINTS) ? MAX_HA_POINTS : capacity;
+                final double[] haValues = new double[haLen];
+                final Date[] dateValues = new Date[haLen];
 
-                        // check safety limit :
-                        if (j >= MAX_HA_POINTS) {
-                            addWarning("Too many HA points (" + capacity + "), check your sampling periodicity. Only " + MAX_HA_POINTS + " samples computed");
-                            break;
+                Range obsRange;
+                int j = 0;
+                for (double ha = haMin; ha <= haMax; ha += step) {
+
+                    // check HA start:
+                    if ((obsRange = Range.find(obsRangesHA, ha, HA_PRECISION)) != null) {
+                        // check HA end:
+                        if (obsRange.contains(ha + haObsTime, HA_PRECISION)) {
+                            haValues[j] = ha;
+                            dateValues[j] = this.sc.toDate(this.sc.convertHAToJD(ha, precRA), this.obsData.isUseLST()); // LST or GMT
+                            j++;
+
+                            // check safety limit :
+                            if (j >= MAX_HA_POINTS) {
+                                addWarning("Too many HA points (" + capacity + "), check your sampling periodicity. Only " + MAX_HA_POINTS + " samples computed");
+                                break;
+                            }
+                        } else if (logger.isDebugEnabled()) {
+                            logger.debug("Observation HA range [{}; {}]end exceed observable range: {}", ha, ha + haObsTime, obsRange);
                         }
-                    } else if (logger.isDebugEnabled()) {
-                        logger.debug("Observation HA range [{}; {}]end exceed observable range: {}", ha, ha + haObsTime, obsRange);
                     }
                 }
-            }
 
-            // correct number of HA points :
-            final int nPoints = j;
+                // correct number of HA points :
+                final int nPoints = j;
 
-            // check if there is at least one observable HA :
-            if (nPoints == 0) {
-                addWarning("Check your HA min/max settings. There is no observable HA");
-                return;
-            }
-
-            final double[] HA = new double[nPoints];
-            System.arraycopy(haValues, 0, HA, 0, nPoints);
-
-            this.data.setHA(HA);
-
-            // Second pass : extract UV values for HA points :
-
-            // precessed target declination in rad :
-            final double precDEC = FastMath.toRadians(this.starData.getPrecDEC());
-
-            final double cosDec = FastMath.cos(precDEC);
-            final double sinDec = FastMath.sin(precDEC);
-
-            final double invLambdaMin = 1d / this.lambdaMin;
-            final double invLambdaMax = 1d / this.lambdaMax;
-
-            final int sizeBL = this.baseLines.size();
-            final List<UVRangeBaseLineData> targetUVObservability = new ArrayList<UVRangeBaseLineData>(sizeBL);
-
-            UVRangeBaseLineData uvData;
-            BaseLine baseLine;
-
-            /* pure U,V coordinates (m) */
-            double[] u;
-            double[] v;
-            /* U,V coordinates corrected with minimal wavelength */
-            double[] uWMin;
-            double[] vWMin;
-            /* U,V coordinates corrected with maximal wavelength */
-            double[] uWMax;
-            double[] vWMax;
-
-            double cosHa, sinHa;
-
-            for (int i = 0; i < sizeBL; i++) {
-                baseLine = this.baseLines.get(i);
-
-                uvData = new UVRangeBaseLineData(baseLine);
-
-                u = new double[nPoints];
-                v = new double[nPoints];
-                uWMin = new double[nPoints];
-                vWMin = new double[nPoints];
-                uWMax = new double[nPoints];
-                vWMax = new double[nPoints];
-
-                for (j = 0; j < nPoints; j++) {
-
-                    sinHa = FastMath.sinAndCos(AngleUtils.hours2rad(HA[j]), cw); // cw holds cosine
-                    cosHa = cw.value;
-
-                    // Baseline projected vector (m) :
-                    u[j] = CalcUVW.computeU(baseLine, cosHa, sinHa);
-                    v[j] = CalcUVW.computeV(cosDec, sinDec, baseLine, cosHa, sinHa);
-
-                    // wavelength correction :
-
-                    // Spatial frequency (rad-1) :
-                    uWMin[j] = u[j] * invLambdaMin;
-                    vWMin[j] = v[j] * invLambdaMin;
-
-                    uWMax[j] = u[j] * invLambdaMax;
-                    vWMax[j] = v[j] * invLambdaMax;
-                }
-
-                uvData.setNPoints(nPoints);
-                uvData.setU(u);
-                uvData.setV(v);
-                uvData.setUWMin(uWMin);
-                uvData.setVWMin(vWMin);
-                uvData.setUWMax(uWMax);
-                uvData.setVWMax(vWMax);
-
-                targetUVObservability.add(uvData);
-
-                // fast interrupt :
-                if (this.currentThread.isInterrupted()) {
+                // check if there is at least one observable HA :
+                if (nPoints == 0) {
+                    addWarning("Check your HA min/max settings. There is no observable HA");
                     return;
                 }
-            }
 
-            this.data.setTargetUVObservability(targetUVObservability);
+                final double[] HA = new double[nPoints];
+                System.arraycopy(haValues, 0, HA, 0, nPoints);
+
+                this.data.setHA(HA);
+                this.data.setDates(dateValues);
+
+                // Second pass : extract UV values for HA points :
+
+                // precessed target declination in rad :
+                final double precDEC = FastMath.toRadians(this.starData.getPrecDEC());
+
+                final double cosDec = FastMath.cos(precDEC);
+                final double sinDec = FastMath.sin(precDEC);
+
+                final double invLambdaMin = 1d / this.lambdaMin;
+                final double invLambdaMax = 1d / this.lambdaMax;
+
+                final int sizeBL = this.baseLines.size();
+                final List<UVRangeBaseLineData> targetUVObservability = new ArrayList<UVRangeBaseLineData>(sizeBL);
+
+                UVRangeBaseLineData uvData;
+                BaseLine baseLine;
+
+                /* pure U,V coordinates (m) */
+                double[] u;
+                double[] v;
+                /* U,V coordinates corrected with minimal wavelength */
+                double[] uWMin;
+                double[] vWMin;
+                /* U,V coordinates corrected with maximal wavelength */
+                double[] uWMax;
+                double[] vWMax;
+
+                double cosHa, sinHa;
+
+                for (int i = 0; i < sizeBL; i++) {
+                    baseLine = this.baseLines.get(i);
+
+                    uvData = new UVRangeBaseLineData(baseLine);
+
+                    u = new double[nPoints];
+                    v = new double[nPoints];
+                    uWMin = new double[nPoints];
+                    vWMin = new double[nPoints];
+                    uWMax = new double[nPoints];
+                    vWMax = new double[nPoints];
+
+                    for (j = 0; j < nPoints; j++) {
+
+                        sinHa = FastMath.sinAndCos(AngleUtils.hours2rad(HA[j]), cw); // cw holds cosine
+                        cosHa = cw.value;
+
+                        // Baseline projected vector (m) :
+                        u[j] = CalcUVW.computeU(baseLine, cosHa, sinHa);
+                        v[j] = CalcUVW.computeV(cosDec, sinDec, baseLine, cosHa, sinHa);
+
+                        // wavelength correction :
+
+                        // Spatial frequency (rad-1) :
+                        uWMin[j] = u[j] * invLambdaMin;
+                        vWMin[j] = v[j] * invLambdaMin;
+
+                        uWMax[j] = u[j] * invLambdaMax;
+                        vWMax[j] = v[j] * invLambdaMax;
+                    }
+
+                    uvData.setNPoints(nPoints);
+                    uvData.setU(u);
+                    uvData.setV(v);
+                    uvData.setUWMin(uWMin);
+                    uvData.setVWMin(vWMin);
+                    uvData.setUWMax(uWMax);
+                    uvData.setVWMax(vWMax);
+
+                    targetUVObservability.add(uvData);
+
+                    // fast interrupt :
+                    if (this.currentThread.isInterrupted()) {
+                        return;
+                    }
+                }
+
+                this.data.setTargetUVObservability(targetUVObservability);
+            }
         }
     }
 
@@ -496,21 +489,6 @@ public final class UVCoverageService {
             logger.debug("lambda: {}", this.lambda);
             logger.debug("lambdaMax: {}", this.lambdaMax);
             logger.debug("nChannels: {}", this.nSpectralChannels);
-        }
-
-        // HA Min / Max :
-        final TargetConfiguration targetConf = this.observation.getTargetConfiguration(targetName);
-        if (targetConf != null) {
-            if (targetConf.getHAMin() != null) {
-                this.haMin = targetConf.getHAMin().doubleValue();
-            }
-            if (targetConf.getHAMax() != null) {
-                this.haMax = targetConf.getHAMax().doubleValue();
-            }
-        }
-        if (logger.isDebugEnabled()) {
-            logger.debug("ha min: {}", this.haMin);
-            logger.debug("ha max: {}", this.haMax);
         }
 
         // hour angle step in decimal hours :
@@ -579,5 +557,133 @@ public final class UVCoverageService {
      */
     private void addWarning(final String msg) {
         this.data.getWarningContainer().addWarningMessage(msg);
+    }
+
+    /**
+     * Compute UV points (observable) inside HA min/max ranges
+     * @param observation observation settings
+     * @param obsData computed observability data
+     * @param starData star data
+     * @param ha hour angle to compute UV points
+     * @return list of uv point couples corresponding to the target observability
+     * 
+     * @throws IllegalStateException if the instrument mode is undefined
+     */
+    public static List<UVRangeBaseLineData> computeUVPoints(final ObservationSetting observation,
+            final ObservabilityData obsData, final StarData starData,
+            final double ha) throws IllegalStateException {
+
+        // Prepare informations:
+
+        // Get baselines :
+        final List<BaseLine> baseLines = obsData.getBaseLines();
+
+        final FocalInstrumentMode insMode = observation.getInstrumentConfiguration().getFocalInstrumentMode();
+        if (insMode == null) {
+            throw new IllegalStateException("The instrumentMode is empty !");
+        }
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("instrumentMode: {}", insMode.getName());
+        }
+
+        // Get wavelength range for the selected instrument mode :
+        final double lambdaMin = AsproConstants.MICRO_METER * insMode.getWaveLengthMin();
+        final double lambdaMax = AsproConstants.MICRO_METER * insMode.getWaveLengthMax();
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("lambdaMin: {}", lambdaMin);
+            logger.debug("lambdaMax: {}", lambdaMax);
+        }
+
+        // Compute UV points at given HA:
+        final List<Range> obsRangesHA = starData.getObsRangesHA();
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("obsRangesHA: {}", obsRangesHA);
+        }
+
+        final List<UVRangeBaseLineData> targetUVObservability;
+        Range obsRange = null;
+
+        if (obsRangesHA != null && (obsRange = Range.find(obsRangesHA, ha, HA_PRECISION)) != null) {
+
+            // extract UV values for HA point:
+
+            // precessed target declination in rad :
+            final double precDEC = FastMath.toRadians(starData.getPrecDEC());
+
+            final double cosDec = FastMath.cos(precDEC);
+            final double sinDec = FastMath.sin(precDEC);
+
+            final double invLambdaMin = 1d / lambdaMin;
+            final double invLambdaMax = 1d / lambdaMax;
+
+            final int sizeBL = baseLines.size();
+            targetUVObservability = new ArrayList<UVRangeBaseLineData>(sizeBL);
+
+            UVRangeBaseLineData uvData;
+            BaseLine baseLine;
+
+            /* pure U,V coordinates (m) */
+            double[] u;
+            double[] v;
+            /* U,V coordinates corrected with minimal wavelength */
+            double[] uWMin;
+            double[] vWMin;
+            /* U,V coordinates corrected with maximal wavelength */
+            double[] uWMax;
+            double[] vWMax;
+
+            double haRad;
+            double cosHa, sinHa;
+
+            for (int i = 0; i < sizeBL; i++) {
+                baseLine = baseLines.get(i);
+
+                uvData = new UVRangeBaseLineData(baseLine);
+
+                u = new double[1];
+                v = new double[1];
+                uWMin = new double[1];
+                vWMin = new double[1];
+                uWMax = new double[1];
+                vWMax = new double[1];
+
+                haRad = AngleUtils.hours2rad(ha);
+                sinHa = FastMath.sin(haRad);
+                cosHa = FastMath.cos(haRad);
+
+                // Baseline projected vector (m) :
+                u[0] = CalcUVW.computeU(baseLine, cosHa, sinHa);
+                v[0] = CalcUVW.computeV(cosDec, sinDec, baseLine, cosHa, sinHa);
+
+                // wavelength correction :
+
+                // Spatial frequency (rad-1) :
+                uWMin[0] = u[0] * invLambdaMin;
+                vWMin[0] = v[0] * invLambdaMin;
+
+                uWMax[0] = u[0] * invLambdaMax;
+                vWMax[0] = v[0] * invLambdaMax;
+
+                uvData.setNPoints(1);
+                uvData.setU(u);
+                uvData.setV(v);
+                uvData.setUWMin(uWMin);
+                uvData.setVWMin(vWMin);
+                uvData.setUWMax(uWMax);
+                uvData.setVWMax(vWMax);
+
+                targetUVObservability.add(uvData);
+            }
+
+        } else {
+            if (logger.isDebugEnabled()) {
+                logger.debug("Observation HA [{}] out of observable range: {}", ha, obsRange);
+            }
+            targetUVObservability = null;
+        }
+        return targetUVObservability;
     }
 }
