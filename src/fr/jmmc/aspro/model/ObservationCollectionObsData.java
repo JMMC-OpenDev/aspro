8979b8c19621b73c519fdb4e1c34c5ebe148345a
bourgesl <bourges.laurent@gmail.com>
2013-06-10 14:35:37 +0000

    compute configuration names even in single mode

--- src/fr/jmmc/aspro/model/ObservationCollectionObsData.java
+++ src/fr/jmmc/aspro/model/ObservationCollectionObsData.java
@@ -14,84 +14,81 @@ import java.util.List;
  */
 public class ObservationCollectionObsData extends ObservationCollection {
 
-  /** observability data */
-  private final List<ObservabilityData> obsDataList;
-  /** configuration names using the format 'CONF + PoPs' */
-  private final List<String> confNames;
+    /** observability data */
+    private final List<ObservabilityData> obsDataList;
+    /** configuration names using the format 'CONF + PoPs' */
+    private final List<String> confNames;
 
-  /**
-   * Public constructor : copy information from the given observation collection (by reference)
-   * @param obsCollection observation collection to copy
-   * @param obsDataList observability data
-   */
-  public ObservationCollectionObsData(final ObservationCollection obsCollection,
-                                      final List<ObservabilityData> obsDataList) {
-    super(obsCollection);
-    this.obsDataList = obsDataList;
+    /**
+     * Public constructor : copy information from the given observation collection (by reference)
+     * @param obsCollection observation collection to copy
+     * @param obsDataList observability data
+     */
+    public ObservationCollectionObsData(final ObservationCollection obsCollection,
+            final List<ObservabilityData> obsDataList) {
+        super(obsCollection);
+        this.obsDataList = obsDataList;
 
-    if (isSingle()) {
-      this.confNames = null;
-    } else {
-      this.confNames = new ArrayList<String>(size());
-      prepareConfigurationNames();
+        this.confNames = new ArrayList<String>(size());
+        prepareConfigurationNames();
     }
-  }
 
-  /**
-   * Prepare the configuration names using the format 'CONF + PoPs'
-   */
-  private final void prepareConfigurationNames() {
-    final StringBuilder sb = new StringBuilder();
+    /**
+     * Prepare the configuration names using the format 'CONF + PoPs'
+     */
+    private final void prepareConfigurationNames() {
+        final StringBuilder sb = new StringBuilder();
 
-    // Iterate over Observability data (multi conf) :
-    for (ObservabilityData obsData : getObsDataList()) {
-      sb.append(obsData.getStationNames());
-      if (!obsData.isUserPops() && obsData.getBestPops() != null) {
-        obsData.getBestPops().toString(sb);
-      }
+        // Iterate over Observability data (multi conf) :
+        for (ObservabilityData obsData : getObsDataList()) {
+            sb.append(obsData.getStationNames());
 
-      this.confNames.add(sb.toString());
-      sb.setLength(0);
+            if (!obsData.isUserPops() && obsData.getBestPops() != null) {
+                obsData.getBestPops().toString(sb);
+            }
+
+            this.confNames.add(sb.toString());
+            sb.setLength(0);
+        }
     }
-  }
 
-  /**
-   * Return the observability data
-   * @return observability data
-   */
-  public final List<ObservabilityData> getObsDataList() {
-    return this.obsDataList;
-  }
+    /**
+     * Return the observability data
+     * @return observability data
+     */
+    public final List<ObservabilityData> getObsDataList() {
+        return this.obsDataList;
+    }
 
-  /**
-   * Return the first observability data
-   * @return first observability data
-   */
-  public final ObservabilityData getFirstObsData() {
-    return this.getObsDataList().get(0);
-  }
+    /**
+     * Return the first observability data
+     * @return first observability data
+     */
+    public final ObservabilityData getFirstObsData() {
+        return this.getObsDataList().get(0);
+    }
 
-  /**
-   * Return the flag to find baseline limits
-   * @return flag to find baseline limits
-   */
-  public final boolean isDoBaseLineLimits() {
-    return this.getFirstObsData().isDoBaseLineLimits();
-  }
+    /**
+     * Return the flag to find baseline limits
+     * @return flag to find baseline limits
+     */
+    public final boolean isDoBaseLineLimits() {
+        return this.getFirstObsData().isDoBaseLineLimits();
+    }
 
-  /**
-   * Return the flag to produce detailed output with all BL / horizon / rise intervals per target
-   * @return flag to produce detailed output with all BL / horizon / rise intervals per target
-   */
-  public final boolean isDoDetailedOutput() {
-    return this.getFirstObsData().isDoDetailedOutput();
-  }
+    /**
+     * Return the flag to produce detailed output with all BL / horizon / rise intervals per target
+     * @return flag to produce detailed output with all BL / horizon / rise intervals per target
+     */
+    public final boolean isDoDetailedOutput() {
+        return this.getFirstObsData().isDoDetailedOutput();
+    }
 
-  /**
-   * Return the configuration names using the format 'CONF + PoPs'
-   * @return the configuration names using the format 'CONF + PoPs'
-   */
-  public final List<String> getConfigurationNames() {
-    return this.confNames;
-  }
+    /**
+     * Return the configuration names using the format 'CONF + PoPs'
+     * @return the configuration names using the format 'CONF + PoPs'
+     */
+    public final List<String> getConfigurationNames() {
+        return this.confNames;
+    }
 }
