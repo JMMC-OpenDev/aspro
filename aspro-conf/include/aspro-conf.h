cf3407e6fb793251434951bcd88b128eff40b6b2
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2013-09-06 13:21:37 +0000

    Automatically tagged ASPRO2 in version ASPRO2_V0_9_5 from trunk  (mkfRepoUtil)

---
+++ aspro-conf/include/aspro-conf.h
@@ -0,0 +1,21 @@
+#ifndef aspro-conf_H
+#define aspro-conf_H
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
+
+/**
+ * @file
+ * aspro-conf general header file.
+ */
+
+
+/*
+ * Local headers
+ */
+
+ 
+
+#endif /*!aspro-conf_H*/
+
+/*___oOo___*/
