aabce63c34c47572831012904b2c7e03aa23b3f9
bourgesl <bourges.laurent@gmail.com>
2014-02-10 16:29:31 +0000

    added draft plugins support (unused)

---
+++ tools/plugins/fr/jmmc/aspro/feature/AsproFeatures.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
+package fr.jmmc.aspro.feature;
+
+/**
+ * This enum describes the Aspro2 features that are implemented by Aspro2 or by an Aspro plugin
+ * @author bourgesl
+ */
+public enum AsproFeatures {
+
+  /** export OB Vega as star list */
+  ExportOBVega,
+  /** instrument's spectral configuration editor (future) */
+  InstrumentModeEditor,
+  /** target's acquisition time editor (future) */
+  TargetAcquisitionTimeEditor;
+}
