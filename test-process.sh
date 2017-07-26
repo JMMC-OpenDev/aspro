f7a599db3b5120cf4edd69e8341dff3809a91107
bourgesl <bourges.laurent@gmail.com>
2017-07-26 08:55:10 +0000

    added test-process shell (amhra)

---
+++ test-process.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR=./src/test/resources/amhra/
+INPUT=Achernar_all_Pionier_oiDataCalib_2012-09-17.fits
+
+IMAGE=aeri_rot_1.65um_hr.fits
+OUTPUT="$INPUT--$IMAGE"
+
+java -Djava.awt.headless -jar target/aspro2-TRUNK-jar-with-dependencies.jar -process -input $DIR/$INPUT -image $DIR/$IMAGE -output $DIR/$OUTPUT
+
+IMAGE=image_disco_amhra.fits
+OUTPUT="$INPUT--$IMAGE"
+
+java -Djava.awt.headless -jar target/aspro2-TRUNK-jar-with-dependencies.jar -process -input $DIR/$INPUT -image $DIR/$IMAGE -output $DIR/$OUTPUT
+
