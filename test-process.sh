fc791314099d88925840fb0144d15ec69e52813f
bourgesl <bourges.laurent@gmail.com>
2017-08-23 14:54:30 +0000

    test UD model 5 mas to have T3phi != 0

--- test-process.sh
+++ test-process.sh
@@ -13,7 +13,8 @@ OUTPUT="$INPUT--$IMAGE"
 
 #java -Djava.awt.headless -jar target/aspro2-TRUNK-jar-with-dependencies.jar -process -input $DIR/$INPUT -image $DIR/$IMAGE -output $DIR/$OUTPUT
 
-IMAGE=image_disco_amhra_UD2mas.fits
+#IMAGE=image_disco_amhra_UD2mas.fits
+IMAGE=image_disco_amhra_UD5mas.fits
 OUTPUT="$INPUT--$IMAGE"
 
 java -Djava.awt.headless -jar target/aspro2-TRUNK-jar-with-dependencies.jar -process -input $DIR/$INPUT -image $DIR/$IMAGE -output $DIR/$OUTPUT
