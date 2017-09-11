689e8b6cc4f77cbe26789c2558e2ad692a9c1b82
bourgesl <bourges.laurent@gmail.com>
2017-09-11 14:22:01 +0000

    bad tag

--- aspro/aspro/src/main/java/fr/jmmc/aspro/model/ob/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.jmmc.fr/aspro-ob/0.1")
-package fr.jmmc.aspro.model.ob;
--- aspro/src/main/java/fr/jmmc/aspro/model/oi/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.jmmc.fr/aspro-oi/0.1")
-package fr.jmmc.aspro.model.oi;
