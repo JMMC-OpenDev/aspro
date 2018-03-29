ee11afaae777f4867dddc61fcc41d53d76f9ad14
bourgesl <bourges.laurent@gmail.com>
2018-03-29 15:34:17 +0000

    code formatting

--- src/main/java/fr/jmmc/aspro/model/util/SpectralBandUtils.java
+++ src/main/java/fr/jmmc/aspro/model/util/SpectralBandUtils.java
@@ -12,48 +12,48 @@ import fr.jmmc.jmal.Band;
  */
 public final class SpectralBandUtils {
 
-  /**
-   * Forbidden constructor
-   */
-  private SpectralBandUtils() {
-    // no-op
-  }
-
-  /**
-   * Find the SpectralBand corresponding to the given Band
-   * @param band band to use
-   * @return SpectralBand or null
-   */
-  public static SpectralBand findBand(final Band band) {
-    switch (band) {
-      case U:
-        // band U not supported
-          throw new IllegalArgumentException("Band "+band + " not supported !");
-      case B:
-        return SpectralBand.B;
-      case V:
-        return SpectralBand.V;
-      case R:
-        return SpectralBand.R;
-      case I:
-        return SpectralBand.I;
-      case J:
-        return SpectralBand.J;
-      case H:
-        return SpectralBand.H;
-      case K:
-        return SpectralBand.K;
-      case L:
-        return SpectralBand.L;
-      case M:
-        return SpectralBand.M;
-      case N:
-        return SpectralBand.N;
-      case Q:
-        // band Q not supported
-          throw new IllegalArgumentException("Band "+band + " not supported !");
-      default:
-        return null;
+    /**
+     * Forbidden constructor
+     */
+    private SpectralBandUtils() {
+        // no-op
     }
-  }
+
+    /**
+     * Find the SpectralBand corresponding to the given Band
+     * @param band band to use
+     * @return SpectralBand or null
+     */
+    public static SpectralBand findBand(final Band band) {
+        switch (band) {
+            case U:
+                // band U not supported
+                throw new IllegalArgumentException("Band " + band + " not supported !");
+            case B:
+                return SpectralBand.B;
+            case V:
+                return SpectralBand.V;
+            case R:
+                return SpectralBand.R;
+            case I:
+                return SpectralBand.I;
+            case J:
+                return SpectralBand.J;
+            case H:
+                return SpectralBand.H;
+            case K:
+                return SpectralBand.K;
+            case L:
+                return SpectralBand.L;
+            case M:
+                return SpectralBand.M;
+            case N:
+                return SpectralBand.N;
+            case Q:
+                // band Q not supported
+                throw new IllegalArgumentException("Band " + band + " not supported !");
+            default:
+                return null;
+        }
+    }
 }
