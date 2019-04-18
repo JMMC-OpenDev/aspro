db0eea9d9124e15bb04295cbb2ca0a5788a83214
bourgesl <bourges.laurent@gmail.com>
2019-04-18 12:07:44 +0000

    Automatically tagged ASPRO2-conf in version ASPRO2-CONF_2019_0329 from trunk  (admManager.sh)

---
+++ aspro-conf/src/main/java/fr/jmmc/aspro/conf/AsproConf.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
+package fr.jmmc.aspro.conf;
+
+import fr.jmmc.jmcs.util.FileUtils;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * This class only provides checksum generation a configuration as input stream
+ * @author bourgesl
+ */
+public final class AsproConf {
+
+    /**
+     * Private constructor (utility class)
+     */
+    private AsproConf() {
+        // no-op
+    }
+
+    /**
+     * Internal: Computes the checksum for the given input stream
+     * @param in input stream to process
+     * @return checksum value as long integer
+     * @throws IOException if any I/O error occurs.
+     */
+    public static long checksum(final InputStream in) throws IOException {
+        long checksum = 1l;
+
+        final byte[] bytes = FileUtils.checksum(in);
+
+        for (byte b : bytes) {
+            long val = b & 0xFF;
+            
+            if (val != 0L) {
+                checksum *= val;
+            }
+        }
+
+        return Math.abs(checksum);
+    }
+}
