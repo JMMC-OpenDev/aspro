241c95cb46c8abb21b3d5f3b246f37ac0e4cd027
bourgesl <bourges.laurent@gmail.com>
2021-10-01 15:50:54 +0200

    fixed export aspro conf for ObsPortal (VLTI / CHARA)

--- src/test/java/fr/jmmc/aspro/model/AsproConfExportModes.java
+++ src/test/java/fr/jmmc/aspro/model/AsproConfExportModes.java
@@ -15,8 +15,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.LinkedHashSet;
 import java.util.Set;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -24,14 +22,11 @@ import org.slf4j.LoggerFactory;
  */
 public class AsproConfExportModes {
 
-    /** Class logger */
-    private static final Logger _logger = LoggerFactory.getLogger(AsproConfExportModes.class.getName());
-
     /** absolute path to test folder to load test resources */
     protected final static String TEST_FOLDER = getProjectFolderPath() + "src/test/resources/";
 
     /**
-     * Export Aspro2 configuration into json format
+     * Export Aspro2 configuration into xml format (obsportal)
      * @param args unused
      */
     public static void main(final String[] args) throws IOException {
@@ -39,69 +34,79 @@ public class AsproConfExportModes {
         // invoke Bootstrapper method to initialize logback now:
         Bootstrapper.getState();
 
+        export("VLTI");
+        export("CHARA");
+    }
+
+    /**
+     * Export the given interferometer into xml format
+     * @param interferometerName
+     */
+    public static void export(String interferometerName) throws IOException {
+
+        final ConfigurationManager cm = ConfigurationManager.getInstance();
+
+        final InterferometerDescription id = cm.getInterferometerDescription(interferometerName);
+
+        if (id == null) {
+            System.err.println("Missing InterferometerDescription = " + interferometerName);
+            return;
+        }
+
         final Set<String> insNames = new LinkedHashSet<String>();
 
         final StringBuilder sb = new StringBuilder(16 * 1024);
         sb.append("<configuration>\n");
 
-        final ConfigurationManager cm = ConfigurationManager.getInstance();
+        sb.append("    <interferometer>\n");
+        sb.append("        <name>").append(id.getName()).append("</name>\n");
 
-        for (String name : cm.getInterferometerNames()) {
-            final InterferometerDescription id = cm.getInterferometerDescription(name);
+        final LonLatAlt position = id.getPosSph();
+        sb.append("        <lat>").append(Math.toDegrees(position.getLatitude())).append("</lat>\n");
+        sb.append("        <lon>").append(Math.toDegrees(position.getLongitude())).append("</lon>\n");
+        sb.append("        <alt>").append(position.getAltitude()).append("</alt>\n");
 
-            if (!("VLTI".equals(id.getName()) || "CHARA".equals(id.getName()))) {
+        for (FocalInstrument ins : id.getFocalInstruments()) {
+            String insName = ins.getName();
+
+            int pos = insName.lastIndexOf('_');
+            if (pos != -1) {
+                final String suffix = insName.substring(pos + 1, insName.length());
+
+                if ((suffix.length() == 2) && Character.isDigit(suffix.charAt(0)) && (suffix.charAt(1) == 'T')) {
+                    // ins name ends with "_?T" = instrument variant:
+                    insName = insName.substring(0, pos);
+                }
+            }
+
+            if (insNames.contains(insName)) {
+                System.out.println("Skipping instrument : " + ins.getName() + " (already processed " + insName + ") !");
                 continue;
+            } else {
+                insNames.add(insName);
             }
 
-            sb.append("    <interferometer>\n");
-            sb.append("        <name>").append(id.getName()).append("</name>\n");
+            System.out.println("Processing instrument : " + insName + " ...");
 
-            final LonLatAlt position = id.getPosSph();
-            sb.append("        <lat>").append(Math.toDegrees(position.getLatitude())).append("</lat>\n");
-            sb.append("        <lon>").append(Math.toDegrees(position.getLongitude())).append("</lon>\n");
-            sb.append("        <alt>").append(position.getAltitude()).append("</alt>\n");
+            sb.append("        <instrument>\n");
+            sb.append("            <name>").append(insName).append("</name>\n");
 
-            for (FocalInstrument ins : id.getFocalInstruments()) {
-                String insName = ins.getName();
-
-                int pos = insName.lastIndexOf('_');
-                if (pos != -1) {
-                    final String suffix = insName.substring(pos + 1, insName.length());
-
-                    if ((suffix.length() == 2) && Character.isDigit(suffix.charAt(0)) && (suffix.charAt(1) == 'T')) {
-                        // ins name ends with "_?T" = instrument variant:
-                        insName = insName.substring(0, pos);
-                    }
-                }
-
-                if (insNames.contains(insName)) {
-                    System.out.println("Skipping instrument : " + ins.getName() + " (already processed " + insName + ") !");
-                    continue;
-                } else {
-                    insNames.add(insName);
-                }
-
-                System.out.println("Processing instrument : " + insName + " ...");
-
-                sb.append("        <instrument>\n");
-                sb.append("            <name>").append(insName).append("</name>\n");
-
-                for (FocalInstrumentMode mode : ins.getModes()) {
-                    sb.append("            <mode>\n");
-                    sb.append("                <name>").append(mode.getName()).append("</name>\n");
-                    sb.append("                <wave_min>").append(NumberUtils.trimTo5Digits(mode.getWaveLengthMin())).append("</wave_min>\n");
-                    sb.append("                <wave_max>").append(NumberUtils.trimTo5Digits(mode.getWaveLengthMax())).append("</wave_max>\n");
-                    sb.append("                <spec_res>").append((int) Math.ceil(mode.getResolution())).append("</spec_res>\n");
-                    sb.append("            </mode>\n");
-                }
-                sb.append("        </instrument>\n");
+            for (FocalInstrumentMode mode : ins.getModes()) {
+                sb.append("            <mode>\n");
+                sb.append("                <name>").append(mode.getName()).append("</name>\n");
+                sb.append("                <wave_min>").append(NumberUtils.trimTo5Digits(mode.getWaveLengthMin())).append("</wave_min>\n");
+                sb.append("                <wave_max>").append(NumberUtils.trimTo5Digits(mode.getWaveLengthMax())).append("</wave_max>\n");
+                sb.append("                <spec_res>").append((int) Math.ceil(mode.getResolution())).append("</spec_res>\n");
+                sb.append("            </mode>\n");
             }
-            sb.append("    </interferometer>\n");
+            sb.append("        </instrument>\n");
         }
+        sb.append("    </interferometer>\n");
+
         sb.append("</configuration>\n");
 
         System.out.println("XML:\n" + sb.toString());
 
-        FileUtils.writeFile(new File(TEST_FOLDER + "aspro-conf.xml"), sb.toString());
+        FileUtils.writeFile(new File(TEST_FOLDER + "aspro-conf-" + interferometerName + ".xml"), sb.toString());
     }
 }
