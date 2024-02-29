d3832fa8b7ff9239e66cad8737ccfeb512d0b1e9
bourgesl <bourges.laurent@gmail.com>
2024-02-29 16:36:36 +0100

    fixed h0 value to match ESO paranal atmosphere (lolo fit)

--- src/main/java/fr/jmmc/aspro/model/util/AtmosphereQualityUtils.java
+++ src/main/java/fr/jmmc/aspro/model/util/AtmosphereQualityUtils.java
@@ -64,6 +64,40 @@ public final class AtmosphereQualityUtils {
         return AtmosphereQuality.fromValue(value);
     }
 
+    /*
+    * Using ESO turbulence categories:
+    - GRAVITY: https://www.eso.org/sci/observing/phase2/ObsConditions.GRAVITY.html
+        More specifically, the categories are:
+                T < 10%, corresponding to seeing ≤ 0.60“ and τ0 > 5.2ms
+                T < 20%, corresponding to seeing ≤ 0.70“ and τ0 > 4.4ms
+                T < 30%, corresponding to seeing ≤ 0.80“ and τ0 > 4.1ms
+                T < 50%, corresponding to seeing ≤ 1.00“ and τ0 > 3.2ms
+                T < 70%, corresponding to seeing ≤ 1.15“ and τ0 > 2.2ms
+                T < 85%, corresponding to seeing ≤ 1.40“ and τ0 > 1.6ms
+        For conditions worse than T = 85%, no GRAVITY operations are possible
+    
+    - MATISSE: https://www.eso.org/sci/observing/phase2/ObsConditions.MATISSE.html
+        More specifically, the categories are:
+            T < 10%, corresponding to seeing ≤ 0.60“ and τ0 > 5.2ms
+            T < 20%, corresponding to seeing ≤ 0.70“ and τ0 > 4.4ms
+            T < 30%, corresponding to seeing ≤ 0.80“ and τ0 > 4.1ms
+            T < 50%, corresponding to seeing ≤ 1.00“ and τ0 > 3.2ms
+            T < 70%, corresponding to seeing ≤ 1.15“ and τ0 > 2.2ms
+            T < 85%, corresponding to seeing ≤ 1.40“ and τ0 > 1.6ms
+        For conditions worse than T = 85%, no MATISSE operations are possible
+    
+    - PIONIER: https://www.eso.org/sci/observing/phase2/ObsConditions.PIONIER.html
+        More specifically, the categories are:
+            T < 10%, corresponding to seeing ≤ 0.60“ and τ0 > 5.2ms
+            T < 20%, corresponding to seeing ≤ 0.70“ and τ0 > 4.4ms
+            T < 30%, corresponding to seeing ≤ 0.80“ and τ0 > 4.1ms
+            T < 50%, corresponding to seeing ≤ 1.00“ and τ0 > 3.2ms
+            T < 70%, corresponding to seeing ≤ 1.15“ and τ0 > 2.2ms
+            T < 85%, corresponding to seeing ≤ 1.40“ and τ0 > 1.6ms
+    For conditions worse than T = 70%, no PIONIER operations are possible
+    
+    So for ASPRO2: last category is T < 85% (GRAVITY-MATISSE - PIONIER)
+     */
     /**
      * Return the corresponding seeing in arc seconds for the given atmosphere quality
      * @param atmQuality atmosphere quality
@@ -72,22 +106,24 @@ public final class AtmosphereQualityUtils {
     public static double getSeeing(final AtmosphereQuality atmQuality) {
         switch (atmQuality) {
             case EXCELLENT:
-                /* "10%  (Seeing < 0.6  arcsec, t0 > 5.2 ms)" */
-                return 0.6;
+                /* T < 10%, corresponding to seeing ≤ 0.60“ and τ0 > 5.2ms */
+                return 0.60;
             case GOOD:
-                /* "20%  (Seeing < 0.7  arcsec, t0 > 4.4 ms)" */
-                return 0.7;
+                /* T < 20%, corresponding to seeing ≤ 0.70“ and τ0 > 4.4ms */
+                return 0.70;
+            /* missing: T < 30%, corresponding to seeing ≤ 0.80“ and τ0 > 4.1ms */
             default:
             case AVERAGE:
-                /* "50%  (Seeing < 1.0  arcsec, t0 > 3.2 ms)" */
-                return 1.0;
+                /* T < 50%, corresponding to seeing ≤ 1.00“ and τ0 > 3.2ms */
+                return 1.00;
             case WORSE:
-                /* "70%  (Seeing < 1.15 arcsec, t0 > 2.2 ms)" */
+                /* T < 70%, corresponding to seeing ≤ 1.15“ and τ0 > 2.2ms */
                 return 1.15;
             case BAD:
-                return 1.4;
+                /* T < 85%, corresponding to seeing ≤ 1.40“ and τ0 > 1.6ms */
+                return 1.40;
             case AWFUL:
-                return 1.8;
+                return 1.80;
         }
     }
 
@@ -99,22 +135,24 @@ public final class AtmosphereQualityUtils {
     public static double getCoherenceTime(final AtmosphereQuality atmQuality) {
         switch (atmQuality) {
             case EXCELLENT:
-                /* "10%  (Seeing < 0.6  arcsec, t0 > 5.2 ms)" */
+                /* T < 10%, corresponding to seeing ≤ 0.60“ and τ0 > 5.2ms */
                 return 5.2;
             case GOOD:
-                /* "20%  (Seeing < 0.7  arcsec, t0 > 4.4 ms)" */
+                /* T < 20%, corresponding to seeing ≤ 0.70“ and τ0 > 4.4ms */
                 return 4.4;
+            /* missing: T < 30%, corresponding to seeing ≤ 0.80“ and τ0 > 4.1ms */
             default:
             case AVERAGE:
-                /* "50%  (Seeing < 1.0  arcsec, t0 > 3.2 ms)" */
+                /* T < 50%, corresponding to seeing ≤ 1.00“ and τ0 > 3.2ms */
                 return 3.2;
             case WORSE:
-                /* "70%  (Seeing < 1.15 arcsec, t0 > 2.2 ms)" */
+                /* T < 70%, corresponding to seeing ≤ 1.15“ and τ0 > 2.2ms */
                 return 2.2;
             case BAD:
+                /* T < 85%, corresponding to seeing ≤ 1.40“ and τ0 > 1.6ms */
                 return 1.6;
             case AWFUL:
-                return 0.5;
+                return 1.0;
         }
     }
 
@@ -124,24 +162,34 @@ public final class AtmosphereQualityUtils {
      * @return turburlence layer's height (h0) in meters
      */
     public static double getTurbulenceHeight(final AtmosphereQuality atmQuality) {
+        /*
+        # from http://archive.eso.org/wdb/wdb/asm/mass_paranal/form:
+        # Median $8 * $10 = median (MASS Turb Altitude [m] * MASS-DIMM Cn2 fraction at ground)
+
+        seeing_values = np.array([0.60, 0.70, 0.80, 1.00, 1.15, 1.40])
+        tau0_values = np.array([5.2, 4.4, 4.1, 3.2, 2.2, 1.6])
+        ho_values = np.array([5850.0, 5250.0, 4650.0, 3700.0, 3200.0, 2700.0])
+         */
         switch (atmQuality) {
             case EXCELLENT:
-                /* "10%  (Seeing < 0.6  arcsec, t0 > 5.2 ms)" */
-                return 3000.0;
+                /* T < 10%, corresponding to seeing ≤ 0.60“ and τ0 > 5.2ms */
+                return 5850.0;
             case GOOD:
-                /* "20%  (Seeing < 0.7  arcsec, t0 > 4.4 ms)" */
-                return 3500.0;
+                /* T < 20%, corresponding to seeing ≤ 0.70“ and τ0 > 4.4ms */
+                return 5250.0;
+            /* missing: T < 30%, corresponding to seeing ≤ 0.80“ and τ0 > 4.1ms */
             default:
             case AVERAGE:
-                /* "50%  (Seeing < 1.0  arcsec, t0 > 3.2 ms)" */
-                return 4300.0;
+                /* T < 50%, corresponding to seeing ≤ 1.00“ and τ0 > 3.2ms */
+                return 3700.0;
             case WORSE:
-                /* "70%  (Seeing < 1.15 arcsec, t0 > 2.2 ms)" */
-                return 6000.0;
+                /* T < 70%, corresponding to seeing ≤ 1.15“ and τ0 > 2.2ms */
+                return 3200.0;
             case BAD:
-                return 8000.0;
+                /* T < 85%, corresponding to seeing ≤ 1.40“ and τ0 > 1.6ms */
+                return 2700.0;
             case AWFUL:
-                return 10000.0;
+                return 2000.0;
         }
     }
 
@@ -155,9 +203,10 @@ public final class AtmosphereQualityUtils {
     }
 
     private static String computeTooltip(final AtmosphereQuality atmQuality) {
-        return atmQuality.value() + " sky conditions: seeing = "
-                + getSeeing(atmQuality) + " arcsec, t0 = "
-                + getCoherenceTime(atmQuality) + " ms";
+        return atmQuality.value() + " sky conditions ["
+                + "seeing = " + getSeeing(atmQuality) + " as, "
+                + "t0 = " + getCoherenceTime(atmQuality) + " ms, "
+                + "h0 = " + getTurbulenceHeight(atmQuality) + " m]";
     }
 
 }
